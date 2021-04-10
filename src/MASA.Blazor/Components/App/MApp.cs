42733bc10615fefc4d9605b109d8c14a63c40073
doddgu <358683537@qq.com>
2021-04-10 15:08:13 +0800

    feat: init

---
+++ src/MASA.Blazor/Components/App/MApp.cs
@@ -0,0 +1,43 @@
+﻿using BlazorComponent;
+using Microsoft.AspNetCore.Components;
+
+namespace MASA.Blazor.Components.App
+{
+    /// <summary>
+    /// Root for application
+    /// </summary>
+    public partial class MApp : BApp
+    {
+        /// <summary>
+        /// Whether to display from left to right
+        /// </summary>
+        [Parameter]
+        public bool LeftToRight { get; set; } = true;
+
+        /// <summary>
+        /// Whether dark theme
+        /// </summary>
+        [Parameter]
+        public bool Dark { get; set; }
+
+        public override void SetComponentClass()
+        {
+            var prefix = "m-application";
+
+            ClassMapper
+                .Add("m-application")
+                .Add("m-application--wrap")
+                .Get(() =>
+                {
+                    var suffix = LeftToRight ? "ltr" : "rtl";
+                    return $"{prefix}--is-{suffix}";
+                })
+                .Get(() =>
+                {
+                    var suffix = Dark ? "dark" : "light";
+                    return $"theme--{suffix}";
+                });
+
+        }
+    }
+}
