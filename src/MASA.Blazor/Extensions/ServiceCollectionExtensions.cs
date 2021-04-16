09879257fd43f8b8f5ae4e6c4e4eb93b075c7c9a
gushoudao <gushoudao@masastack.com>
2021-04-16 10:11:47 +0800

    feat: theme(unfinished)

---
+++ src/MASA.Blazor/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BlazorComponent;
+using MASA.Blazor;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMasaBlazor(this IServiceCollection services, MasaBlazorOptions options = null)
+        {
+            InitBlazorComponentVariables(options);
+
+            services.AddBlazorComponent();
+
+            return services;
+        }
+
+        private static void InitBlazorComponentVariables(MasaBlazorOptions options)
+        {
+            options ??= new MasaBlazorOptions();
+
+            Variables.DarkTheme = options.DarkTheme;
+            Variables.Theme = options.Theme;
+        }
+    }
+}
---
+++ src/MASA.Blazor/Model/MasaBlazorOptions.cs
@@ -0,0 +1,29 @@
+﻿using BlazorComponent;
+using System.Collections.Generic;
+
+namespace MASA.Blazor
+{
+    public class MasaBlazorOptions
+    {
+        /// <summary>
+        /// Default is false
+        /// </summary>
+        public bool DarkTheme { get; set; }
+
+        /// <summary>
+        /// The theme options
+        /// </summary>
+        public ThemeOptions Theme { get; set; } = new ThemeOptions()
+        {
+            CombinePrefix = ".m-application",
+            Primary = "#1976D2",
+            Secondary = "#424242",
+            Accent = "#82B1FF",
+            Error = "#FF5252",
+            Info = "#2196F3",
+            Success = "#4CAF50",
+            Warning = "#FFC107",
+            UserDefined = new Dictionary<string, string>() { { "Tertiary", "#e57373" } },
+        };
+    }
+}
