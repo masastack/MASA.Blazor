f135d92add0e1980d3e11d60acdf9fafed0197c1
wangzengxing <1625322170@qq.com>
2021-04-16 16:15:18 +0800

    feat:新增container、row、col

---
+++ src/MASA.Blazor/Components/Grid/MCol.cs
@@ -0,0 +1,43 @@
+﻿using BlazorComponent;
+using Microsoft.AspNetCore.Components;
+using OneOf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASA.Blazor.Components.Grid
+{
+    public partial class MCol : BCol
+    {
+        [Parameter]
+        public string AlignSelf { get; set; }
+
+        public override void SetComponentClass()
+        {
+            CssBuilder
+                .AddIf("col", () => IsNullOrDefault(Span))
+                .AddIf($"col-{Span}", () => !IsNullOrDefault(Span))
+                .AddIf($"offset-{Offset}", () => !IsNullOrDefault(Offset))
+                .AddIf($"order-{Order}", () => !IsNullOrDefault(Order))
+                //'auto', 'start', 'end', 'center', 'baseline', 'stretch'
+                .AddIf($"align-self-{AlignSelf}", () => !string.IsNullOrEmpty(AlignSelf));
+        }
+
+        private bool IsNullOrDefault(OneOf<string, int> span)
+        {
+            if (span.IsT0)
+            {
+                return string.IsNullOrEmpty(span.AsT0);
+            }
+
+            if (span.IsT1)
+            {
+                return span.AsT1 == default;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/MASA.Blazor/Components/Grid/MContainer.cs
@@ -0,0 +1,23 @@
+﻿using BlazorComponent;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASA.Blazor
+{
+    public partial class MContainer : BContainer
+    {
+        [Parameter]
+        public bool Fluid { get; set; }
+
+        public override void SetComponentClass()
+        {
+            CssBuilder
+                .Add("container")
+                .AddIf("container--fluid", () => Fluid);
+        }
+    }
+}
