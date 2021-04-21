2d0c0ba335fd16d1a72561fc2c8bf571d8830265
朱嵘 <zhurong@masastack.com>
2021-04-21 04:57:23 +0000

    Feature/card

---
+++ src/Doc/MASA.Blazor.Doc/Demos/Components/Card/doc/index.zh-CN.md
@@ -0,0 +1,11 @@
+---
+category: Components
+subtitle: 卡片
+type: 卡片
+title: Card
+cols: 1
+cover: https://gw.alipayobjects.com/zos/alicdn/rrwbSt3FQ/Icon.svg
+
+---
+
+MCard 组件是一个可用于从面板到静态图像的多功能组件。 卡 组件有许多帮助程序组件来尽可能简单地进行标记。 没有列出选项的组件使用Vue的功能组件来更快渲染并充当标记糖以使建筑变得更加容易。
---
+++ src/MASA.Blazor/Components/Card/MCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorComponent;
+
+namespace MASA.Blazor
+{
+    public partial class MCard : BCard
+    {
+        public override void SetComponentClass()
+        {
+            CssBuilder.Add("m-card").Add("elevation-2");
+
+            CardCoverCssBuilder.Add("m-responsive").Add("m-image__image--cover");
+
+            CoverStyleBuilder.Add("height: 250px;");
+
+            TitleCssBuilder.Add("m-card__title");
+
+            SubTitleCssBuilder.Add("m-card__subtitle");
+
+            TextCssBuilder.Add("m-card__text");
+
+            ActionsCssBuilder.Add("m-card__actions");
+        }
+    }
+}
