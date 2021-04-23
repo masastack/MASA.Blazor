1d0f9e1307559ff5d8b08024177017de4e5cc4ad
朱嵘 <zhurong@masastack.com>
2021-04-23 03:48:23 +0000

    feat: tree

---
+++ src/MASA.Blazor/Components/Tree/MTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorComponent;
+using Microsoft.AspNetCore.Components;
+
+namespace MASA.Blazor
+{
+    public class MTree<T> : BTree<T>
+    {
+        /// <summary>
+        /// Whether dark theme
+        /// </summary>
+        [Parameter]
+        public bool Dark { get; set; }
+
+        public override void SetComponentClass()
+        {
+            CssBuilder.Add("m-treeview").AddTheme(Dark);
+
+            CssBuilderChildren.Add("m-treeview-node__children");
+        }
+    }
+}
---
+++ src/MASA.Blazor/Components/Tree/MTreeItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorComponent;
+
+namespace MASA.Blazor
+{
+    public class MTreeItem<T> : BTreeItem<T>
+    {
+        public override void SetComponentClass()
+        {
+            CssBuilder.Add("m-treeview-node");
+
+            CssBuilderRoot.Add("m-treeview-node__root");
+
+            CssBuilderLevel.Add("m-treeview-node__level");
+
+            
+        }
+    }
+}
