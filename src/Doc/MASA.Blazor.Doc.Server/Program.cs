b74dd8e002d255b74828a8873bba100d755e10da
朱嵘 <zhurong@masastack.com>
2021-04-26 12:22:13 +0000

    Update Program.cs

--- src/Doc/MASA.Blazor.Doc.Server/Program.cs
+++ src/Doc/MASA.Blazor.Doc.Server/Program.cs
@@ -20,6 +20,7 @@ namespace MASA.Blazor.Doc.Server
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseStaticWebAssets();
                     webBuilder.UseStartup<Startup>();
                 });
     }
