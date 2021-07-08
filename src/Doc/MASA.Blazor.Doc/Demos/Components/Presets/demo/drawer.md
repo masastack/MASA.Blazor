ec0128f713fedf3c857906cacfca7a3a990afa0a
caoyouxian <capdiem@live.com>
2021-07-08 14:39:34 +0800

    feat(presets): 🆕 Add a preset component FormModal

--- src/Doc/MASA.Blazor.Doc/Demos/Components/Presets/demo/drawer.md
+++ src/Doc/MASA.Blazor.Doc/Demos/Components/Presets/demo/drawer.md
@@ -1,5 +1,5 @@
 ---
-order: 2
+order: 3
 title:
   zh-CN: 抽屉
   en-US: Drawer
---
+++ src/MASA.Blazor/Presets/FormModal/FormModal.razor.cs
@@ -0,0 +1,59 @@
+﻿using BlazorComponent;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MASA.Blazor.Presets
+{
+    public partial class FormModal
+    {
+        private MForm _form;
+
+        [Parameter]
+        public bool Visible { get; set; }
+
+        [Parameter]
+        public StringNumber Width { get; set; }
+
+        [Parameter]
+        public StringNumber MaxWidth { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string OkText { get; set; } = "确定";
+
+        [Parameter]
+        public string CancelText { get; set; } = "取消";
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnOk { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnCancel { get; set; }
+
+        [Parameter]
+        public object Model { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> Attributes { get; set; }
+
+        private async Task HandleOnOk(MouseEventArgs args)
+        {
+            if (OnOk.HasDelegate)
+            {
+                var context = _form.UseEditContext();
+                if (context.Validate())
+                {
+                    await OnOk.InvokeAsync(args);
+                }
+            }
+        }
+    }
+}
