94d094c02af42437542e64e1aeddcceff66d9949
yanpengju <yanpengju@masastack.com>
2021-11-05 16:39:53 +0800

    chore:修改tooltip

--- src/MASA.Blazor/Components/ColorPicker/MColorPickerPreview.cs
+++ src/MASA.Blazor/Components/ColorPicker/MColorPickerPreview.cs
@@ -19,6 +19,8 @@ namespace MASA.Blazor
         [Parameter]
         public ColorPickerColor Color { get; set; }
 
+        public Dictionary<string, object> HueAttrs => throw new NotImplementedException();
+
         protected override void SetComponentClass()
         {
             var prefix = "m-color-picker";
