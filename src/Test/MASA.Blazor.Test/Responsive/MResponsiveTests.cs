64534816c63421e2faff10974753d884ffec517e
dongxinyang <dongxinyang@lonsid.cn>
2021-11-15 17:00:07 +0800

    feat:Responsive单元测试

--- src/Test/MASA.Blazor.Test/Responsive/MResponsiveTests.cs
+++ src/Test/MASA.Blazor.Test/Responsive/MResponsiveTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bunit;
 
 namespace MASA.Blazor.Test.Responsive
 {
@@ -19,9 +20,99 @@ namespace MASA.Blazor.Test.Responsive
                 props.Add(rating => rating.AspectRatio, 16/9D);
             });
             var classes = cut.Instance.CssProvider.GetClass();
-            var hasReadonlyClass = classes.Contains("m-rating--readonly");
+            var hasReadonlyClass = classes.Contains("m-responsive");
             // Assert
             Assert.IsTrue(hasReadonlyClass);
+        }//m-rating--readonly
+
+        [TestMethod]
+        public void RenderWithHeight()
+        {
+            // Act
+            var cut = RenderComponent<MResponsive>(props =>
+            {
+                props.Add(p => p.Height, 100);
+            });
+            var inputSlotDiv = cut.Find(".m-responsive");
+            var style = inputSlotDiv.GetAttribute("style");
+
+            // Assert
+            Assert.AreEqual("height: 100px", style);
+        }
+
+        [TestMethod]
+        public void RenderWithWidth()
+        {
+            // Act
+            var cut = RenderComponent<MResponsive>(props =>
+            {
+                props.Add(p => p.Width, 100);
+            });
+            var inputSlotDiv = cut.Find(".m-responsive");
+            var style = inputSlotDiv.GetAttribute("style");
+
+            // Assert
+            Assert.AreEqual("width: 100px", style);
+        }
+
+        [TestMethod]
+        public void RenderWithMinWidth()
+        {
+            // Act
+            var cut = RenderComponent<MResponsive>(props =>
+            {
+                props.Add(p => p.MinWidth, 100);
+            });
+            var inputSlotDiv = cut.Find(".m-responsive");
+            var style = inputSlotDiv.GetAttribute("style");
+
+            // Assert
+            Assert.AreEqual("min-width: 100px", style);
+        }
+
+        [TestMethod]
+        public void RenderWithMaxWidth()
+        {
+            // Act
+            var cut = RenderComponent<MResponsive>(props =>
+            {
+                props.Add(p => p.MaxWidth, 100);
+            });
+            var inputSlotDiv = cut.Find(".m-responsive");
+            var style = inputSlotDiv.GetAttribute("style");
+
+            // Assert
+            Assert.AreEqual("max-width: 100px", style);
+        }
+
+        [TestMethod]
+        public void RenderWithMinHeight()
+        {
+            // Act
+            var cut = RenderComponent<MResponsive>(props =>
+            {
+                props.Add(p => p.MinHeight, 100);
+            });
+            var inputSlotDiv = cut.Find(".m-responsive");
+            var style = inputSlotDiv.GetAttribute("style");
+
+            // Assert
+            Assert.AreEqual("min-height: 100px", style);
+        }
+
+        [TestMethod]
+        public void RenderWithMaxHeight()
+        {
+            // Act
+            var cut = RenderComponent<MResponsive>(props =>
+            {
+                props.Add(p => p.MaxHeight, 100);
+            });
+            var inputSlotDiv = cut.Find(".m-responsive");
+            var style = inputSlotDiv.GetAttribute("style");
+
+            // Assert
+            Assert.AreEqual("max-height: 100px", style);
         }
     }
 }
