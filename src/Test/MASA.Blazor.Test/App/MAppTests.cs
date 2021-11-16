70b06b5bef68e004294a25cdb3d677de70bac066
dongxinyang <dongxinyang@lonsid.cn>
2021-11-16 14:43:11 +0800

    feat:修改app单元测试

--- src/Test/MASA.Blazor.Test/App/MAppTests.cs
+++ src/Test/MASA.Blazor.Test/App/MAppTests.cs
@@ -28,54 +28,6 @@ namespace MASA.Blazor.Test.App
             // Assert
             contentDiv.Children.MarkupMatches("<span>Hello world</span>");
         }
-
-        [TestMethod]
-        public void RenderAppWithDark()
-        {
-            //Act
-            JSInterop.Mode = JSRuntimeMode.Loose;
-            var cut = RenderComponent<MApp>(props =>
-            {
-                props.Add(app => app.Dark, true);
-            });
-            var classes = cut.Instance.CssProvider.GetClass();
-            var hasDarkClass = classes.Contains("theme--dark");
-
-            // Assert
-            Assert.IsTrue(hasDarkClass);
-        }
-
-        [TestMethod]
-        public void RendeAppWithLight()
-        {
-            //Act
-            JSInterop.Mode = JSRuntimeMode.Loose;
-            var cut = RenderComponent<MApp>(props =>
-            {
-                props.Add(app => app.Light, true);
-            });
-            var classes = cut.Instance.CssProvider.GetClass();
-            var hasLightClass = classes.Contains("theme--light");
-
-            // Assert
-            Assert.IsTrue(hasLightClass);
-        }
-
-        [TestMethod]
-        public void RendeAppWithLeftToRight()
-        {
-            //Act
-            JSInterop.Mode = JSRuntimeMode.Loose;
-            var cut = RenderComponent<MApp>(props =>
-            {
-                props.Add(app => app.LeftToRight, true);
-            });
-            var classes = cut.Instance.CssProvider.GetClass();
-            var hasLeftToRightClass = classes.Contains("m-application");
-
-            // Assert
-            Assert.IsTrue(hasLeftToRightClass);
-        }
     }
 
 }
