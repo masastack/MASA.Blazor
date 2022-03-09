49ff75aa5a4a30fb3a69e15a9a874ac9943b856f
capdiem <capdiem@live.com>
2022-03-09 17:40:41 +0800

    ♻ refactor: ok -> save

--- src/Masa.Blazor/Presets/Modal/ModalActionEventArgs.cs
+++ src/Masa.Blazor/Presets/Modal/ModalActionEventArgs.cs
@@ -13,7 +13,7 @@ public class ModalActionEventArgs
     /// Cancel the next operation.
     /// </summary>
     /// <remarks>
-    /// The form would be reset when Form's OnOk is triggered,
+    /// The form would be reset when Form's OnSave is triggered,
     /// you can call this method to prevent this behavior.
     /// </remarks>
     public void Cancel()
