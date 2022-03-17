364543ebfb7fc276c3fc4f84c391e823b5ce51f9
wangzengxing <1625322170@qq.com>
2022-03-17 17:34:53 +0800

    fix(input):fixed validation not work

--- src/Masa.Blazor/Components/Select/MSelect.cs
+++ src/Masa.Blazor/Components/Select/MSelect.cs
@@ -302,7 +302,7 @@ namespace Masa.Blazor
             {
                 if (value is TValue val)
                 {
-                    InternalValue = val;
+                    await SetInternalValueAsync(val);
                 }
                 IsMenuActive = false;
             }
@@ -320,7 +320,7 @@ namespace Masa.Blazor
 
                 if (internalValues is TValue val)
                 {
-                    InternalValue = val;
+                    await SetInternalValueAsync(val);
                 }
             }
 
@@ -398,7 +398,7 @@ namespace Masa.Blazor
             if (Multiple)
             {
                 IList<TItemValue> values = new List<TItemValue>();
-                InternalValue = (TValue)values;
+                await SetInternalValueAsync((TValue)values);
             }
             else
             {
