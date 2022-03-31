4deea4182ee61751b38f7489b9aa6d2613a2a204
吴自浩 <wuzihao425@outlook.com>
2022-03-31 22:06:10 +0800

    fix：Remove image Base64 upload and add upload configuration

---
+++ src/Masa.Blazor/Components/Editor/MEditorUpload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masa.Blazor.Components.Editor
+{
+    public class MEditorUpload
+    {
+        public string Url { get; set; }
+        public string Methods { get; set; } = "POST";
+        public string Token { get; set; }
+        public string TokenName { get; set; }
+        public string Name { get; set; } = "file";
+        public string Accept { get; set; } = "image/png, image/gif, image/jpeg, image/bmp, image/x-icon";
+    }
+}
