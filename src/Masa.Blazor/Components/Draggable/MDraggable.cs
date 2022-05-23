6967dbaf8daf253326f65ab6dbd3e679c35e651e
qinyouzeng <qinyouzeng@masastack.com>
2022-05-23 17:48:46 +0800

    feat: drap&drop add

---
+++ src/Masa.Blazor/Components/Draggable/MDraggable.cs
@@ -0,0 +1,51 @@
+﻿namespace Masa.Blazor
+{
+    public partial class MDraggable<Item> : BDragzone<Item>
+    {
+        //protected override void SetComponentClass()
+        //{
+        //    CssProvider
+        //        .Apply("wrapper", css => { },
+        //            style => { style.Add("position:relative;display:inherit;").Add(WrapperStyle); })
+        //        .Apply(css =>
+        //        {
+        //            css.Add("m-border")
+        //                .Add(() => Border switch
+        //                {
+        //                    Borders.Left => "m-border__left",
+        //                    Borders.Right => "m-border__right",
+        //                    Borders.Top => "m-border__top",
+        //                    Borders.Bottom => "m-border__bottom",
+        //                    _ => "",
+        //                })
+        //                .AddColor(Color)
+        //                .AddRounded(Rounded);
+        //        }, style =>
+        //        {
+        //            style
+        //                .AddIf("display:none", () => Inactive)
+        //                .AddIf(() =>
+        //                {
+        //                    var (number, unit) = ComputedWidth();
+        //                    return number > 0 ? $"border-width:{number}{unit}" : "";
+        //                }, () => Width != null)
+        //                .AddIf(() =>
+        //                {
+        //                    var (number, unit) = ComputedWidth();
+        //                    if (number <= 0) return "";
+
+        //                    var calc = $"calc(1px - {number * 2}{unit})";
+
+        //                    return Border switch
+        //                    {
+        //                        Borders.Left => $"left:{calc}",
+        //                        Borders.Right => $"right:{calc}",
+        //                        Borders.Top => $"top:{calc}",
+        //                        Borders.Bottom => $"bottom:{calc}",
+        //                        _ => "",
+        //                    };
+        //                }, () => Offset);
+        //        });
+        //}
+    }
+}
---
+++ src/Masa.Blazor/wwwroot/js/draggable/sorttable-helper.js
@@ -0,0 +1,44 @@
+﻿export function init(containerId, option) {
+    let _options = {
+        group: null,
+        disabled: false,
+        handle: ".my-handle",
+        draggable: ".item",
+        onStart: function (evt) {
+            log("onStart", evt);
+        },
+        onEnd: function (evt) {
+            log("onEnd", evt);
+        },
+        onAdd: function (evt) {
+            log("onAdd", evt);
+        },
+        onUpdate: function (evt) {
+            log("onUpdate", evt);
+        },
+        onSort: function (evt) {
+            log("onSort", evt);
+        },
+        onRemove: function (evt) {
+            log("onRemove", evt);
+        },
+        onMove: function (evt) {
+            log("onMove", evt);
+        },
+        onClone: function (evt) {
+            log("onClone", evt);
+        },
+        onChange: function (evt) {
+            log("onChange", evt);
+        },
+    }
+    if (Sortable && containerId) {
+        new Sortable(document.getElementById(containerId), Object.assign(_options, option))
+    }
+
+    function log(name, obj) {
+        //debugger
+        console.log(`${name} evt:${JSON.stringify(obj)}`);
+    }
+
+}
