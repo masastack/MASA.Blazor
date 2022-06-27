2e33671bc2aa36ef7f7c14d194ee55dac531a751
capdiem <capdiem@live.com>
2022-06-28 00:59:59 +0800

    ♻ refactor: add IMasaBlazorBuilder interface fot better organizational injection

---
+++ src/Masa.Blazor/Extensions/DependencyInjection/MasaBlazorBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public class MasaBlazorBuilder : BlazorComponentBuilder, IMasaBlazorBuilder
+{
+    public MasaBlazorBuilder(IServiceCollection services) : base(services)
+    {
+    }
+}
