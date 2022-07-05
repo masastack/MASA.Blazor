772eff44c860d87efc442701797f981918152a9c
capdiem <capdiem@live.com>
2022-07-05 17:27:11 +0800

    🐛 fix(css): extend css was not written to min css (#558)

--- src/Masa.Blazor/gulpfile.js
+++ src/Masa.Blazor/gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
+var cssimport = require("gulp-cssimport");
 var minifyCSS = require("gulp-minify-css");
 var rename = require("gulp-rename");
 
 gulp.task("min", function () {
   return gulp
     .src("wwwroot/css/masa-blazor.css")
+    .pipe(cssimport())
     .pipe(rename({ suffix: ".min" }))
     .pipe(minifyCSS())
     .pipe(gulp.dest("wwwroot/css"));
