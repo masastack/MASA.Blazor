cc5b2481a039bd3c347e5fc0e9b02c5ab05e07b0
capdiem <capdiem@live.com>
2022-08-05 13:46:32 +0800

    ♻ refactor: remove the debug code

--- src/Masa.Blazor/Components/ECharts/MECharts.cs
+++ src/Masa.Blazor/Components/ECharts/MECharts.cs
@@ -1,118 +1,115 @@
 ﻿using BlazorComponent.Web;
 
-namespace Masa.Blazor
+namespace Masa.Blazor;
+
+public class MECharts : BECharts, IDisposable
 {
-    public class MECharts : BECharts, IDisposable
+    [Inject]
+    protected I18n I18n { get; set; }
+
+    [Parameter]
+    public StringNumber Width { get; set; } = 600;
+
+    [Parameter]
+    public StringNumber Height { get; set; } = 400;
+
+    [Parameter]
+    public StringNumber MinWidth { get; set; }
+
+    [Parameter]
+    public StringNumber MinHeight { get; set; }
+
+    [Parameter]
+    public StringNumber MaxWidth { get; set; }
+
+    [Parameter]
+    public StringNumber MaxHeight { get; set; }
+
+    [Parameter]
+    public Action<EChartsInitOptions> InitOptions { get; set; }
+
+    [Parameter]
+    public object Option { get; set; } = new { };
+
+    [Parameter]
+    public bool Light { get; set; }
+
+    [Parameter]
+    public bool Dark { get; set; }
+
+    [Parameter]
+    public string Theme { get; set; }
+
+    [CascadingParameter(Name = "IsDark")]
+    public bool CascadingIsDark { get; set; }
+
+    private EChartsInitOptions DefaultInitOptions { get; set; } = new();
+
+    private IJSObjectReference _echarts;
+    private bool _isEChartsDisposed = false;
+
+    public string ComputedTheme
     {
-        [Inject]
-        protected I18n I18n { get; set; }
-
-        [Parameter]
-        public StringNumber Width { get; set; } = 600;
-
-        [Parameter]
-        public StringNumber Height { get; set; } = 400;
-
-        [Parameter]
-        public StringNumber MinWidth { get; set; }
-
-        [Parameter]
-        public StringNumber MinHeight { get; set; }
-
-        [Parameter]
-        public StringNumber MaxWidth { get; set; }
-
-        [Parameter]
-        public StringNumber MaxHeight { get; set; }
-
-        [Parameter]
-        public Action<EChartsInitOptions> InitOptions { get; set; }
-
-        [Parameter]
-        public object Option { get; set; } = new { };
-
-        [Parameter]
-        public bool Light { get; set; }
-
-        [Parameter]
-        public bool Dark { get; set; }
-
-        [Parameter]
-        public string Theme { get; set; }
-
-        [CascadingParameter(Name = "IsDark")]
-        public bool CascadingIsDark { get; set; }
-
-        private EChartsInitOptions DefaultInitOptions { get; set; } = new();
-
-        private IJSObjectReference _echarts;
-        private bool _isEChartsDisposed = false;
-
-        public string ComputedTheme
+        get
         {
-            get
+            if (Theme is not null)
             {
-                if (Theme is not null)
-                {
-                    return Theme;
-                }
-
-                if (Dark)
-                {
-                    return "dark";
-                }
-
-                return null;
-            }
-        }
-
-        protected override void SetComponentClass()
-        {
-            CssProvider
-                .Apply(styleAction: styleBuilder =>
-                {
-                    styleBuilder
-                        .AddWidth(Width)
-                        .AddHeight(Height)
-                        .AddMinWidth(MinWidth)
-                        .AddMinHeight(MinHeight)
-                        .AddMaxHeight(MaxHeight)
-                        .AddMaxWidth(MaxWidth);
-                });
-        }
-
-        protected override void OnParametersSet()
-        {
-            Console.WriteLine($"CascadingIsDark: {CascadingIsDark}");
-
-            InitOptions?.Invoke(DefaultInitOptions);
-
-            DefaultInitOptions.Locale ??= I18n.Culture.TwoLetterISOLanguageName.ToUpperInvariant();
-        }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (IsDisposed)
-            {
-                return;
+                return Theme;
             }
 
-            if (firstRender)
+            if (Dark)
             {
-                _echarts = await Js.InvokeAsync<IJSObjectReference>("import", "./_content/Masa.Blazor/js/echarts-helper.js");
+                return "dark";
             }
 
-            if (firstRender || _isEChartsDisposed)
-            {
-                _isEChartsDisposed = false;
-                await _echarts.InvokeVoidAsync("init", Ref.GetSelector(), ComputedTheme, DefaultInitOptions, Option);
-            }
+            return null;
         }
+    }
 
-        public async Task DisposeEcharts()
+    protected override void SetComponentClass()
+    {
+        CssProvider
+            .Apply(styleAction: styleBuilder =>
+            {
+                styleBuilder
+                    .AddWidth(Width)
+                    .AddHeight(Height)
+                    .AddMinWidth(MinWidth)
+                    .AddMinHeight(MinHeight)
+                    .AddMaxHeight(MaxHeight)
+                    .AddMaxWidth(MaxWidth);
+            });
+    }
+
+    protected override void OnParametersSet()
+    {
+        InitOptions?.Invoke(DefaultInitOptions);
+
+        DefaultInitOptions.Locale ??= I18n.Culture.TwoLetterISOLanguageName.ToUpperInvariant();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (IsDisposed)
         {
-            _isEChartsDisposed = true;
-            await _echarts.InvokeVoidAsync("dispose", Ref.GetSelector());
+            return;
         }
+
+        if (firstRender)
+        {
+            _echarts = await Js.InvokeAsync<IJSObjectReference>("import", "./_content/Masa.Blazor/js/echarts-helper.js");
+        }
+
+        if (firstRender || _isEChartsDisposed)
+        {
+            _isEChartsDisposed = false;
+            await _echarts.InvokeVoidAsync("init", Ref.GetSelector(), ComputedTheme, DefaultInitOptions, Option);
+        }
+    }
+
+    public async Task DisposeEcharts()
+    {
+        _isEChartsDisposed = true;
+        await _echarts.InvokeVoidAsync("dispose", Ref.GetSelector());
     }
 }
