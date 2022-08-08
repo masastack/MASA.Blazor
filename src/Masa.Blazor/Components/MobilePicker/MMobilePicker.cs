17abefff9626b77647194f8235148ca508755c28
capdiem <capdiem@live.com>
2022-08-09 01:21:10 +0800

    🚧 feat: wip

--- src/Masa.Blazor/Components/MobilePicker/MMobilePicker.cs
+++ src/Masa.Blazor/Components/MobilePicker/MMobilePicker.cs
@@ -22,23 +22,17 @@ public partial class MMobilePicker<TItem, TItemValue> : BMobilePicker<TItem, TIt
             .Apply("toolbar", cssBuilder => { cssBuilder.Add("m-mobile-picker__toolbar"); })
             .Apply("title", cssBuilder => { cssBuilder.Add("m-mobile-picker__title"); })
             .Apply("columns", cssBuilder => { cssBuilder.Add("m-mobile-picker__columns"); },
-                styleBuilder => { styleBuilder.AddHeight(ComputedHeight); })
-            .Apply("column", cssBuilder => { cssBuilder.Add("m-mobile-picker__column"); })
-            .Apply("column-wrapper", cssBuilder => { cssBuilder.Add("m-mobile-picker__column-wrapper"); },
-                styleBuilder => { styleBuilder.Add(() => $"transform:"); })
-            .Apply("column-item",
-                cssBuilder => { cssBuilder.Add("m-mobile-picker__column-item"); },
-                styleBuilder => { styleBuilder.AddHeight(ItemHeight); })
-            .Apply("column-text", cssBuilder => { cssBuilder.Add("m-mobile-picker__column-text"); })
+                styleBuilder => { styleBuilder.AddHeight(WrapHeight); })
             .Apply("picked", cssBuilder => { cssBuilder.Add("m-mobile-picker__picked"); }, styleBuilder =>
             {
                 styleBuilder.AddHeight(ItemHeight)
                             .AddTop(ComputedTop);
             })
             .Apply("mask", cssBuilder => { cssBuilder.Add("m-mobile-picker__mask"); },
-                styleBuilder => { styleBuilder.Add(() => $"backgroundSize: 100% {(ComputedHeight - ComputedItemHeight) / 2}px"); });
+                styleBuilder => { styleBuilder.Add(() => $"backgroundSize: 100% {(WrapHeight - ItemPxHeight) / 2}px"); });
 
         AbstractProvider
+            .Apply(typeof(BMobilePickerColumn<>), typeof(MMobilePickerColumn<TItem>))
             .Apply<BButton, MButton>("cancel", attrs =>
             {
                 attrs[nameof(MButton.Text)] = true;
---
+++ src/Masa.Blazor/Components/MobilePicker/MMobilePickerColumn.cs
@@ -0,0 +1,18 @@
+﻿namespace Masa.Blazor;
+
+public partial class MMobilePickerColumn<TColumnItem> : BMobilePickerColumn<TColumnItem>
+{
+    protected override void SetComponentClass()
+    {
+        base.SetComponentClass();
+
+        CssProvider
+            .Apply("column", cssBuilder => { cssBuilder.Add("m-mobile-picker__column"); })
+            .Apply("column-wrapper", cssBuilder => { cssBuilder.Add("m-mobile-picker__column-wrapper"); },
+                styleBuilder => { styleBuilder.Add(() => $"transform:"); })
+            .Apply("column-item",
+                cssBuilder => { cssBuilder.Add("m-mobile-picker__column-item"); },
+                styleBuilder => { styleBuilder.AddHeight(ItemHeight); })
+            .Apply("column-text", cssBuilder => { cssBuilder.Add("m-mobile-picker__column-text"); });
+    }
+}
