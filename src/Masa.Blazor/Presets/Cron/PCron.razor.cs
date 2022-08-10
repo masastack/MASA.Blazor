8fd596da94d7c5d1775845dc31bda9c21d6276fa
Gavin <kevin_1ee@163.com>
2022-08-10 14:22:22 +0800

    fix(PCron): PCron value can modify by manual

--- src/Masa.Blazor/Presets/Cron/PCron.razor.cs
+++ src/Masa.Blazor/Presets/Cron/PCron.razor.cs
@@ -6,10 +6,26 @@ public partial class PCron
     I18n I18n { get; set; }
 
     [Parameter]
-    public string Value { get; set; } = string.Empty;
+    public string Value
+    {
+        get
+        {
+            return _value;
+        }
+        set
+        {
+            if(_value != value)
+            {
+                _value = value;
+                OnValueChanged();
+            }
+        }
+    }
 
     [Parameter]
     public EventCallback<string> ValueChanged { get; set; }
+
+    private string _value;
 
     private bool _hasError;
 
@@ -40,6 +56,30 @@ public partial class PCron
         await CalculateCronValue();
 
         await base.OnInitializedAsync();
+    }
+
+    private Task OnValueChanged()
+    {
+        if (CronExpression.IsValidExpression(_value))
+        {
+            var valueArr = _value.Split(" ");
+            var cronItemsCount = CronItems.Count;
+            for (int i = 0; i < valueArr.Length; i++)
+            {
+                if (cronItemsCount < i + 1)
+                {
+                    return Task.CompletedTask;
+                }
+                CronItems[i].CronValue = valueArr[i];
+            }
+
+            if (ValueChanged.HasDelegate)
+            {
+                ValueChanged.InvokeAsync(_value);
+            }
+        }
+
+        return Task.CompletedTask;
     }
 
     private Task CronValueHasChanged(CronItemModel cronItem)
@@ -116,12 +156,6 @@ public partial class PCron
             _hasError = true;
             _errorMessage = I18n.T(ex.Message);
         }
-
-        if (ValueChanged.HasDelegate)
-        {
-            ValueChanged.InvokeAsync(Value);
-        }
-
         return Task.CompletedTask;
     }
 }
