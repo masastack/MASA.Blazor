40abb39f84a54c0348edcbdb5e924d7e45b82f56
capdiem <capdiem@live.com>
2022-08-12 18:30:09 +0800

    🚧 feat: wip

---
+++ src/Masa.Blazor/Components/InfiniteScroll/MInfiniteScroll.cs
@@ -0,0 +1,17 @@
+﻿namespace Masa.Blazor;
+
+public partial class MInfiniteScroll : BInfiniteScroll
+{
+    [Parameter] public bool Dense { get; set; }
+
+    protected override void SetComponentClass()
+    {
+        base.SetComponentClass();
+
+        CssProvider
+            .Apply(cssBuilder => { cssBuilder.Add("m-infinite-scroll"); })
+            .Apply("text--no-more", cssBuilder => { cssBuilder.Add("m-infinite-scroll__text--no-more"); })
+            .Apply("text--loading", cssBuilder => { cssBuilder.Add("m-infinite-scroll__text--loading"); })
+            .Apply("text--failed", cssBuilder => { cssBuilder.Add("m-infinite-scroll__text--failed"); });
+    }
+}
