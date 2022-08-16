68ab7948759d49f13eddd8d2381441fc695d8016
capdiem <capdiem@live.com>
2022-08-16 16:20:05 +0800

    🆕 feat: support i18n

--- src/Masa.Blazor/Components/InfiniteScroll/MInfiniteScroll.cs
+++ src/Masa.Blazor/Components/InfiniteScroll/MInfiniteScroll.cs
@@ -2,6 +2,19 @@
 
 public partial class MInfiniteScroll : BInfiniteScroll
 {
+    [Inject]
+    protected I18n I18n { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        NoMoreText ??= I18n.T("$masaBlazor.infiniteScroll.noMore");
+        FailedToLoadText ??= I18n.T("$masaBlazor.infiniteScroll.failedToLoad");
+        ReloadText ??= I18n.T("$masaBlazor.infiniteScroll.reload");
+        LoadingText ??= I18n.T("$masaBlazor.infiniteScroll.loading");
+    }
+
     protected override void SetComponentClass()
     {
         base.SetComponentClass();
