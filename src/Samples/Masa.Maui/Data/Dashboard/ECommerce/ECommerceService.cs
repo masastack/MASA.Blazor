cb63a6b9d5134014a62f03a5e06d412b67ed5e29
鬼谷子 <358683537@qq.com>
2022-09-09 12:58:45 +0800

    Update src/Samples/Masa.Maui/Data/Dashboard/ECommerce/ECommerceService.cs
    Co-authored-by: Jeremy Wu <1280442096@qq.com>

--- src/Samples/Masa.Maui/Data/Dashboard/ECommerce/ECommerceService.cs
+++ src/Samples/Masa.Maui/Data/Dashboard/ECommerce/ECommerceService.cs
@@ -4,11 +4,11 @@ public static class ECommerceService
 {
     public static List<CompanyDto> GetCompanyList() => new List<CompanyDto>()
     {
-        new("Dixons","Technology","23.4k","$891.2","68%","/img/avatar/1.svg","meguc@ruj.io",1,"in 24 hours",false),
-        new("Motels","Grocery","78k","$668.51","97%","/img/avatar/2.svg","vecav@hodzi.co.uk",2,"in 2 days",true),
-        new("Zipcar","Fashion","162","$522.29","62%","/img/avatar/3.svg","davcilse@is.gov",3,"in 5 days",true),
-        new("Owning","Technology","214","$291.01","88%","/img/avatar/4.svg","meguc@ruj.io",1,"in 24 hours",true),
-        new("Cafés","Grocery","208","$783.93","16%","/img/avatar/7.svg","meguc@ruj.io",2,"in 24 hours",false),
+        new("Dixons", "Technology", "23.4k", "$891.2", "68%", "/img/avatar/1.svg", "meguc@ruj.io", 1, "in 24 hours", false),
+        new("Motels", "Grocery", "78k", "$668.51", "97%", "/img/avatar/2.svg", "vecav@hodzi.co.uk", 2, "in 2 days", true),
+        new("Zipcar", "Fashion", "162", "$522.29", "62%", "/img/avatar/3.svg", "davcilse@is.gov", 3, "in 5 days", true),
+        new("Owning", "Technology", "214", "$291.01", "88%", "/img/avatar/4.svg", "meguc@ruj.io", 1, "in 24 hours", true),
+        new("Cafés", "Grocery", "208", "$783.93", "16%", "/img/avatar/7.svg", "meguc@ruj.io", 2, "in 24 hours", false),
     };
 
     public static int[][] GetOrderChartData() => new int[][] { new[] { 0, 0 }, new[] { 1, 2 }, new[] { 2, 1 }, new[] { 3, 3 }, new[] { 4, 2 }, new[] { 5, 4 } };
