c79c7d4ebfdb80bcd6cc353707bf2f3af5e16abb
LONSID\guohao <guohao@masastack.com>
2022-10-26 17:37:53 +0800

    add fingerprint service

---
+++ src/Masa.Blazor.Maui.Plugin/Masa.Blazor.Maui.Plugin.Biometrics/Platforms/Android/MasaMauiAuthCallback.cs
@@ -0,0 +1,35 @@
+﻿using AndroidX.Core.Hardware.Fingerprint;
+
+namespace Masa.Blazor.Maui.Plugin.Biometrics
+{
+    public class MasaMauiAuthCallback : FingerprintManagerCompat.AuthenticationCallback
+    {
+        //  Write whatever you want, but keep it unique within the app
+        byte[] SECRET_BYTES = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        
+        public override void OnAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result)
+        {
+            if (result.CryptoObject.Cipher != null) 
+            {
+                var doFinalResult = result.CryptoObject.Cipher.DoFinal(SECRET_BYTES);
+
+                if (doFinalResult.Any())
+                {
+                    MessagingCenter.Send<MasaMauiAuthCallback,string>(this, "Validation", "Validation succeeded");
+
+                }
+            }
+            else
+            {
+                // Not using Cipher?
+                // Our example here uses Cipher, leaving aside the exclusions for now
+            }
+        }
+
+        public override void OnAuthenticationFailed()
+        {
+            // Notifies the user that authentication failed
+            MessagingCenter.Send<MasaMauiAuthCallback, string>(this, "Validation", "Validation failed");
+        }
+    }
+}
---
+++ src/Masa.Blazor.Maui.Plugin/Masa.Blazor.Maui.Plugin.Biometrics/Platforms/iOS/MasaMauiFingerprintService.cs
@@ -0,0 +1,7 @@
+﻿namespace Masa.Blazor.Maui.Plugin.Biometrics
+{
+    // All the code in this file is only included on iOS.
+    public static partial class MasaMauiFingerprintService
+    {
+    }
+}
