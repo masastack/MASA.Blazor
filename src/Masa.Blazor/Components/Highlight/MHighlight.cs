7c6321cf5233ec78db6d623914af03574d2277d8
capdiem <capdiem@live.com>
2022-10-31 23:40:00 +0800

    🆕 FEAT: Add Highlight component

---
+++ src/Masa.Blazor/Components/Highlight/MHighlight.cs
@@ -0,0 +1,19 @@
+﻿namespace Masa.Blazor;
+
+public partial class MHighlight : BHighlight
+{
+    protected override void SetComponentClass()
+    {
+        base.SetComponentClass();
+
+        CssProvider
+            .Apply(css =>
+            {
+                css.Add("m-code-highlight")
+                   .AddIf($"language-{Language.ToLower()}", () => Language is not null);
+            }).Apply("code", css =>
+            {
+                css.AddIf($"language-{Language.ToLower()}", () => Language is not null);
+            });
+    }
+}
