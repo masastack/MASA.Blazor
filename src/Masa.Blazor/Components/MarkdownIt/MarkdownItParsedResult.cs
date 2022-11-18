cdf520969dc871fb8c4c0ef01a1b12d2f1feaf22
capdiem <capdiem@live.com>
2022-11-18 16:52:06 +0800

    ♻ refactor: move MarkdownIt from BlazorComponent to Masa.Blazor and refactor it (#796)

---
+++ src/Masa.Blazor/Components/MarkdownIt/MarkdownItParsedResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Masa.Blazor;
+
+public class MarkdownItParsedResult
+{
+    public string? FrontMatter { get; set; }
+
+    [JsonPropertyName("markup")]
+    public string MarkupContent { get; set; }
+
+    public List<MarkdownItTocContent> Toc { get; set; }
+}
