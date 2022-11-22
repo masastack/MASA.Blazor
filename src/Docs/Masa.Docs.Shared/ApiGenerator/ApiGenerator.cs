32d428348187866c6f0a352481aa2d1e034fb364
wuweilai <824255785@qq.com>
2022-11-22 17:14:00 +0800

    add default compontens

--- src/Docs/Masa.Docs.Shared/ApiGenerator/ApiGenerator.cs
+++ src/Docs/Masa.Docs.Shared/ApiGenerator/ApiGenerator.cs
@@ -1,6 +1,9 @@
 ﻿using BlazorComponent.Attributes;
 using OneOf;
 using System.Reflection;
+using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace Masa.Docs.Shared.ApiGenerator;
@@ -46,6 +49,45 @@ public static class ApiGenerator
             };
 
             parametersCache.Add(typeName, value);
+        }
+
+        var json = File.ReadAllText("D:\\wuweilai\\project\\MASA.Blazor\\src\\Docs\\Masa.Docs.Shared\\wwwroot\\data\\apis\\common.json"); //_httpClient.GetFromJsonAsync<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>("_content/Masa.Docs.Shared/data/apis/common.json").Result ?? new();
+        var _commonApis = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>(json);
+        var inrskeys = new List<string>();
+        foreach(var (key,value) in _commonApis["zh-CN"])
+        {
+            foreach(var (key2,value2) in value)
+            {
+                inrskeys.Add(key2.ToUpper());
+            }
+        }
+        var jsonOption = new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+        var jsonMap = new Dictionary<string, Dictionary<string, string>>();
+        foreach (var parameter in parametersCache)
+        {
+            var directory = "";
+            if (parameter.Key.StartsWith("M")) directory = parameter.Key.TrimStart('M');
+            else if (parameter.Key.StartsWith("P")) directory = parameter.Key.TrimStart('P');
+            else continue;
+            directory = char.ToLower(directory[0]) + directory.Substring(1) + "s";
+            var path = $"D:\\wuweilai\\project\\MASA.Blazor\\src\\Docs\\Masa.Docs.Shared\\wwwroot\\data\\apis\\{directory}";
+            if (Directory.Exists(path)) continue;
+            else
+            {
+                foreach (var (key, value) in parameter.Value)
+                {
+                    jsonMap.Add(key, value.Where(v => inrskeys.Contains(v.Name.ToUpper()) is false).ToDictionary(p => p.Name, p => p.Description ?? ""));
+                }
+                Directory.CreateDirectory(path);
+                File.WriteAllText($"{path}/zh-CN.json", JsonSerializer.Serialize(jsonMap, jsonOption));
+                File.WriteAllText($"{path}/en-US.json", JsonSerializer.Serialize(jsonMap, jsonOption));
+            }
+            jsonMap.Clear();
         }
     }
 
