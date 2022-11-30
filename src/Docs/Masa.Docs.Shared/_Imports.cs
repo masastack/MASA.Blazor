d96886b956cf5f65773c0f0116c33aabfc8f270c
yanpengju <yanpengju@masastack.com>
2022-11-30 11:18:19 +0800

    feat(right-nav): add right nav style and get github issues data

--- src/Docs/Masa.Docs.Shared/_Imports.cs
+++ src/Docs/Masa.Docs.Shared/_Imports.cs
@@ -4,3 +4,4 @@ global using Masa.Blazor;
 global using Masa.Docs.Shared.JsonConverters;
 global using Masa.Docs.Shared.Services;
 global using Microsoft.AspNetCore.Components;
+global using Microsoft.Extensions.Caching.Memory;
