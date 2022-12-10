760807c4f1c07fd5e0f8108e2ed308f8b811752f
dkx4m8w <External.Hao.Guo@vwfsag.com>
2022-12-11 00:46:23 +0800

    add Badger for ios

---
+++ src/Masa.Blazor.Maui.Plugin/Masa.Blazor.Maui.Plugin.Badger/Platforms/iOS/MasaMauiBadgerService.cs
@@ -0,0 +1,15 @@
+﻿using UIKit;
+using UserNotifications;
+namespace Masa.Blazor.Maui.Plugin.Badger
+{
+		// All the code in this file is only included on iOS.
+	public static partial class MasaMauiBadgerService
+	{
+		public static void PlatformSetNotificationCount(int count)
+		{
+			UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Badge, (r, e) =>{});
+			UIApplication.SharedApplication.ApplicationIconBadgeNumber = count;
+		}
+	}
+
+}
