a1234851849a1e31af78638ee69ee02a1ae246b4
dkx4m8w <External.Hao.Guo@vwfsag.com>
2022-12-11 09:50:08 +0800

    add Add a comment

--- src/Masa.Blazor.Maui.Plugin/Masa.Blazor.Maui.Plugin.Badger/Platforms/iOS/MasaMauiBadgerService.cs
+++ src/Masa.Blazor.Maui.Plugin/Masa.Blazor.Maui.Plugin.Badger/Platforms/iOS/MasaMauiBadgerService.cs
@@ -7,7 +7,11 @@ namespace Masa.Blazor.Maui.Plugin.Badger
 	{
 		public static void PlatformSetNotificationCount(int count)
 		{
+			// Requests the user’s authorization to allow local and remote notifications for your app.
 			UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Badge, (r, e) =>{});
+
+			// The number currently set as the badge of the app icon on the Home screen
+			// Set to 0 (zero) to hide the badge number. The default value of this property is 0.
 			UIApplication.SharedApplication.ApplicationIconBadgeNumber = count;
 		}
 	}
