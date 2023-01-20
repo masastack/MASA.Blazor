35b2b4674f5cd63cbe0c5e70d09af6550377c0ef
capdiem <capdiem@live.com>
2023-01-20 20:30:11 +0800

    ♻ refactor(ApiGenerator): use SourceGenerator to generate the api metadata

--- src/Docs/Masa.Blazor.Docs/_Imports.cs
+++ src/Docs/Masa.Blazor.Docs/_Imports.cs
@@ -1,6 +1,7 @@
 ﻿global using BlazorComponent;
 global using Masa.Blazor;
 global using Microsoft.AspNetCore.Components;
+global using Masa.Blazor.Docs;
 global using Masa.Blazor.Docs.Services;
 global using Masa.Docs.Core;
 global using Masa.Docs.Core.Services;
