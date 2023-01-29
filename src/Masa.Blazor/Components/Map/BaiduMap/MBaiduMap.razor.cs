5cbc46c7f903ca73fe554728e334fcbf17536822
邓昕茗 <85274323+msnoshain@users.noreply.github.com>
2023-01-29 15:19:51 +0800

    feat: Add MBaiduMap component (#951)
    * feat: Add MBaiduMap component
    
    * Correct namespace
    
    Co-authored-by: capdiem <capdiem@live.com>
    
    * Optimize init params delivery; Isolate map instance; Forbid throwing exceptions
    
    * Revert "Optimize init params delivery; Isolate map instance; Forbid throwing exceptions"
    
    This reverts commit d919b6b14de272c78d1322e21d9be7f012d7cc0d.
    
    * Revert "Revert "Optimize init params delivery; Isolate map instance; Forbid throwing exceptions""
    
    This reverts commit a1ab4d143d8fec1b1be42be90d7a9424aab117d0.
    
    * Optimize init params delivery; Isolate map instance; Forbid throwing exceptions
    
    * Rebuild css & js; Receive map from js; Watch some properties
    
    * Correct watched property
    
    * Optimize params delivery; Correct watched properties; Clean playground
    
    * Rebuild js
    
    * Rename properties
    
    * Rename proeprties; Optimize script injection logic
    
    * Support dark theme; Remove injection codes
    
    * doc: Add docs for MBaiduMap
    
    * doc: Add api docs for baidumaps in en-US
    
    * optimize watchers; update docs
    
    * Correct docs
    
    * Rename variables and methods; Update docs, apis and examples;
    
    * Remove unused variable
    
    * Update param type; Remove unused struct
    
    * Remove reference of PointF
    
    * Update MBaiduMap.razor.cs
    
    * Remove ununsed methods
    
    * Clean playground
    
    ---------
    
    Co-authored-by: capdiem <capdiem@live.com>

---
+++ src/Masa.Blazor/Components/Map/BaiduMap/MBaiduMap.razor.cs
@@ -0,0 +1,198 @@
+﻿using BlazorComponent.Attributes;
+using System.Drawing;
+using System.Text.Json;
+
+namespace Masa.Blazor
+{
+    public partial class MBaiduMap : BDomComponentBase, IThemeable, IMap, IAsyncDisposable
+    {
+        [Inject]
+        public BaiduMapJSModule Module { get; set; }
+
+        [Parameter]
+        [DefaultValue(360)]
+        public StringNumber Width { get; set; } = 360;
+
+        [Parameter]
+        [DefaultValue(240)]
+        public StringNumber Height { get; set; } = 240;
+
+        [Parameter]
+        [DefaultValue(10)]
+        public float Zoom
+        {
+            get => GetValue<float>(10);
+            set
+            {
+                if (value >= DefaultMinZoom && value <= DefaultMaxZoom)
+                    SetValue(value);
+            }
+        }
+
+        [Parameter]
+        [DefaultValue("116.403, 39.917")]
+        public GeoPoint Center
+        {
+            get => GetValue<GeoPoint>(new(116.403f, 39.917f));
+            set => SetValue(value);
+        }
+
+        [Parameter]
+        [DefaultValue(false)]
+        public bool EnableScrollWheelZoom
+        {
+            get => GetValue(false);
+            set => SetValue(value);
+        }
+
+        [Parameter]
+        public string DarkThemeId { get; set; }
+
+        [Parameter]
+        public bool Dark { get; set; }
+
+        [Parameter]
+        public bool Light { get; set; }
+
+        [CascadingParameter(Name = "IsDark")]
+        public bool CascadingIsDark { get; set; }
+
+        public bool IsDark
+        {
+            get
+            {
+                if (Dark)
+                {
+                    return true;
+                }
+
+                if (Light)
+                {
+                    return false;
+                }
+
+                return CascadingIsDark;
+            }
+        }
+
+        private IJSObjectReference _jsMap;
+
+        public static float DefaultMaxZoom { get; } = 19;
+
+        public static float DefaultMinZoom { get; } = 3;
+
+        private bool _zoomChangedInJs = false;
+
+        private bool _centerChangedInJs = false;
+
+        private DotNetObjectReference<MBaiduMap> _objRef;
+
+        protected override void SetComponentClass()
+        {
+            base.SetComponentClass();
+
+            CssProvider
+                .Apply(cssBuilder =>
+                {
+                    cssBuilder
+                        .Add("m-baidumap")
+                        .AddTheme(IsDark);
+                }, styleBuilder =>
+                {
+                    styleBuilder
+                        .AddWidth(Width)
+                        .AddHeight(Height);
+                });
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                _objRef = DotNetObjectReference.Create(this);
+
+                _jsMap = await Module.InitAsync(Id, new BaiduMapInitOptions()
+                {
+                    EnableScrollWheelZoom = EnableScrollWheelZoom,
+                    Zoom = Zoom,
+                    Center = Center,
+                    DarkThemeId = DarkThemeId,
+                    Dark = Dark,
+                }, _objRef);
+            }
+        }
+
+        protected override void OnWatcherInitialized()
+        {
+            base.OnWatcherInitialized();
+
+            Watcher.Watch<float>(nameof(Zoom), async (val) =>
+            {
+                if (_jsMap is null)
+                    return;
+
+                if (_zoomChangedInJs)
+                {
+                    _zoomChangedInJs = false;
+                    return;
+                }
+
+                await _jsMap.InvokeVoidAsync("setZoom", val);
+            });
+
+            Watcher.Watch<bool>(nameof(EnableScrollWheelZoom), async (val) =>
+            {
+                if (_jsMap is null)
+                    return;
+
+                await _jsMap.InvokeVoidAsync(val ? "enableScrollWheelZoom" : "disableScrollWheelZoom");
+            });
+
+            Watcher.Watch<bool>(nameof(Dark), async (val) =>
+            {
+                if (_jsMap is null)
+                    return;
+
+                await _jsMap.InvokeVoidAsync("setMapStyleV2", new { StyleId = val ? DarkThemeId : string.Empty });
+            });
+
+            Watcher.Watch<GeoPoint>(nameof(Center), async (val) =>
+            {
+                if (_jsMap is null)
+                    return;
+
+                if (_centerChangedInJs)
+                {
+                    _centerChangedInJs = false;
+                    return;
+                }
+
+                await _jsMap.InvokeVoidAsync("panTo", val);
+            });
+        }
+
+        [JSInvokable]
+        public void OnJsZoomEnd(float zoom)
+        {
+            _zoomChangedInJs = true;
+            Zoom = zoom;
+        }
+
+        [JSInvokable]
+        public void OnJsMoveEnd(GeoPoint point)
+        {
+            _centerChangedInJs = true;
+            Center = point;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            _objRef?.Dispose();
+
+            if (_jsMap is not null)
+                await _jsMap.DisposeAsync();
+        }
+    }
+}
