0db2f6c1da18089be9853281728ea4993b51f44d
capdiem <capdiem@live.com>
2023-02-03 15:44:04 +0800

    🆕 feat(I18n): Add I18n component (#945)
    * 🆕 feat(I18n): Add I18n component for library, and add contribute components for docs
    
    * 📝 docs(i18n-component): add docs

---
+++ src/Masa.Blazor/Components/I18n/MI18n.cs
@@ -0,0 +1,11 @@
+﻿namespace Masa.Blazor;
+
+public partial class MI18n : BI18n
+{
+    protected override void SetComponentClass()
+    {
+        base.SetComponentClass();
+
+        CssProvider.Apply(cssBuilder => { cssBuilder.Add("m-i18n"); });
+    }
+}
