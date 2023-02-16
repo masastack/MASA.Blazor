4559f7a258aa0e2f794d59980b7031e4808e8d1b
chenyong <chenyong@firmooinc.com>
2023-02-16 22:39:16 +0800

    新增EnumRadioGropu组件，用于直接绑定枚举

---
+++ src/Masa.Blazor/Presets/EnumChip/Tools/EnumExtentions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masa.Blazor.Presets.EnumChip.Tools
+{
+    public static class EnumExtentions
+    {
+
+        /// <summary>
+        /// 获取枚举值的显示值[Display(name="")],如果没有就默认值
+        /// </summary>
+        /// <param name="enum"></param>
+        /// <returns></returns>
+        public static string DisplayText(this Enum @enum)
+        {
+            var t_type = @enum.GetType();
+            var fieldName = Enum.GetName(t_type, @enum);
+            var attributes = t_type.GetField(fieldName).GetCustomAttributes(false);
+            var enumDisplayAttribute = attributes.FirstOrDefault(p => p.GetType().Equals(typeof(DisplayAttribute))) as DisplayAttribute;
+            return enumDisplayAttribute == null ? fieldName : enumDisplayAttribute.Name;
+        }
+    }
+}
