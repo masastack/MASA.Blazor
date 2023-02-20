c44cf3c54027f17c72e31ad933f42b3d40cda856
capdiem <capdiem@live.com>
2023-02-20 15:52:31 +0800

    📝 docs: complete the release-notes page

--- docs/Masa.Docs.Shared/wwwroot/js/docs.js
+++ docs/Masa.Docs.Shared/wwwroot/js/docs.js
@@ -107,8 +107,15 @@ window.MasaBlazor.markdownItRules = function (parser) {
 
   function addLinkRules({ md, defaultSlugify }) {
     md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
-      const next = tokens[idx + 1];
-      const content = next.content;
+      let i = 1;
+      let next = tokens[idx + i];
+      let content = next.content;
+
+      while(!content && next.type !== "link_close") {
+        i++;
+        next = tokens[idx+i]
+        content = next.content
+      }
 
       tokens[idx].tag = "app-link";
 
