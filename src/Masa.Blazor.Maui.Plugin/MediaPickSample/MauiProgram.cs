7e0ac4b7ddd1b4c47bf87e371188165a3faffd1a
sunday866 <49613021+sunday866@users.noreply.github.com>
2023-02-28 12:21:20 +0800

    Feature/maui (#1042)
    * add MediaPickSample
    
    * Compact code
    
    * Compact code
    
    * Optimize the code
    
    * fix android 13 ble bug for device name is empty
    
    * fix bug in device only add
    
    * add ios push sample
    
    ---------
    
    Co-authored-by: dkx4m8w <External.Hao.Guo@vwfsag.com>
    Co-authored-by: LONSID\guohao <guohao@masastack.com>

---
+++ src/Masa.Blazor.Maui.Plugin/MediaPickSample/MauiProgram.cs
@@ -0,0 +1,55 @@
+﻿using Masa.Contrib.Storage.ObjectStorage.Aliyun.Options;
+
+#if ANDROID
+using MediaPickSample.PlatformsAndroid;
+#endif
+#if IOS
+using MediaPickSample.PlatformsIOS;
+#endif
+using MediaPickSample.Service;
+using Microsoft.Extensions.Logging;
+
+namespace MediaPickSample
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if ANDROID
+            builder.Services.AddSingleton<IPhotoPickerService, AndroidPhotoPickerService>();
+#endif
+#if IOS
+builder.Services.AddSingleton<IPhotoPickerService, IOSPhotoPickerService>();
+#endif
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+            builder.Services.AddMasaBlazor();
+            builder.Services.AddAliyunStorage(() =>
+            {
+                return new AliyunStorageOptions(
+                            "accessKeyId",
+                            "accessKeySecret",
+                            "oss-cn-hangzhou.aliyuncs.com",
+                            "roleArn",
+                            "roleSessionName")
+                {
+                    Sts = new AliyunStsOptions("oss-cn-hangzhou")
+                };
+ 
+            });
+            return builder.Build();
+        }
+    }
+}
---
+++ src/Masa.Blazor.Maui.Plugin/MediaPickSample/Platforms/Android/AndroidPhotoPickerService.cs
@@ -0,0 +1,86 @@
+﻿using Android.Content;
+using Android.Provider;
+using AndroidX.Activity.Result;
+using AndroidX.Activity.Result.Contract;
+using MediaPickSample.PlatformsAndroid;
+using MediaPickSample.Service;
+
+[assembly: Dependency(typeof(AndroidPhotoPickerService))]
+namespace MediaPickSample.PlatformsAndroid
+{
+    public class AndroidPhotoPickerService : IPhotoPickerService
+    {
+        public async Task<Dictionary<string, string>> GetImageAsync1()
+        {
+            var dic = new Dictionary<string, string>();
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+
+                if (photo != null)
+                {
+                    using (Stream sourceStream = await photo.OpenReadAsync())
+                    {
+                        sourceStream.Seek(0, SeekOrigin.Begin);
+                        byte[] bs = new byte[sourceStream.Length];
+                        int log = Convert.ToInt32(sourceStream.Length);
+                        sourceStream.Read(bs, 0, log);
+                        var base64Str = Convert.ToBase64String(bs);
+                        dic.Add(photo.FileName, base64Str);
+                    }
+                }
+            }
+            return dic;
+        }
+        public async Task<Dictionary<string, string>> GetImageAsync2()
+        {
+            var dic = new Dictionary<string, string>();
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                var photoList = await FilePicker.Default.PickMultipleAsync(new()
+                {
+                    PickerTitle = "Please select a image file",
+                    FileTypes = FilePickerFileType.Images,
+                });
+
+
+                if (photoList != null && photoList.Any())
+                {
+                    foreach ( var photo in photoList)
+                    {
+                        using (Stream sourceStream = await photo.OpenReadAsync())
+                        {
+                            sourceStream.Seek(0, SeekOrigin.Begin);
+                            byte[] bs = new byte[sourceStream.Length];
+                            int log = Convert.ToInt32(sourceStream.Length);
+                            sourceStream.Read(bs, 0, log);
+                            var base64Str = Convert.ToBase64String(bs);
+                            dic.Add(photo.FileName, base64Str);
+                        }
+                    }
+
+                }
+            }
+            return dic;
+        }
+        public  Task<Dictionary<string, string>> GetImageAsync3()
+        {
+            Intent intent = new Intent(Intent.ActionPick);
+            intent.SetDataAndType(MediaStore.Images.Media.ExternalContentUri, "image/*");
+            intent.PutExtra(Intent.ExtraAllowMultiple,true);
+            MainActivity.Instance.StartActivityForResult(Intent.CreateChooser(intent, "Select Picture"),
+                MainActivity.PickImageId);
+            MainActivity.Instance.PickImageTaskCompletionSource = new TaskCompletionSource<Dictionary<string, string>>();
+            return MainActivity.Instance.PickImageTaskCompletionSource.Task;
+        }
+
+        public Task<Dictionary<string, string>> GetImageAsync4()
+        {
+            MainActivity.PickMultipleMedia.Launch(new PickVisualMediaRequest.Builder()
+                .SetMediaType(ActivityResultContracts.PickVisualMedia.ImageAndVideo.Instance).Build());
+            MainActivity.Instance.PickImageTaskCompletionSource = new TaskCompletionSource<Dictionary<string, string>>();
+           
+            return MainActivity.Instance.PickImageTaskCompletionSource.Task;
+        }
+    }
+}
