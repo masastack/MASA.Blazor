5a685d7a9933d03cb1b0ac6f168ea899092f22a3
Li Ming <1028139084@qq.com>
2023-03-03 16:59:28 +0800

    fix(MRadioGroup): MradioGroup's Value changed, corresponding 'MRadio' not selected

--- src/Masa.Blazor/Components/Radio/MRadioGroup.cs
+++ src/Masa.Blazor/Components/Radio/MRadioGroup.cs
@@ -13,6 +13,12 @@
 
         private List<IRadio<TValue>> Items { get; } = new();
 
+        protected override void OnValueChanged(TValue val)
+        {
+            base.OnValueChanged(val);
+            this.Toggle(val).Wait();
+        }
+
         protected override void SetComponentClass()
         {
             base.SetComponentClass();
