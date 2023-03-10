a89c13502ec6894f29166e877ecc45cc51be5b7c
capdiem <capdiem@live.com>
2023-03-10 20:28:50 +0800

    🚧 feat: wip

---
+++ src/Masa.Blazor.Playground/Pages/PWindowContent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Masa.Blazor.Playground.Pages;
+
+public class PWindowContent : ComponentBase
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    protected override bool ShouldRender() => false;
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.AddContent(0, ChildContent);
+    }
+}
