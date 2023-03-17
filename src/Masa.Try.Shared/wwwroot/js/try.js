0df1f298e8e09c42e67dd7aa2e1e7931767ba763
token <61819790+239573049@users.noreply.github.com>
2023-03-17 17:10:11 +0800

    增加Masa 组件部分语法支持 增加Github文件自动获取

--- src/Masa.Try.Shared/wwwroot/js/try.js
+++ src/Masa.Try.Shared/wwwroot/js/try.js
@@ -1,5 +1,6 @@
 ﻿        
 function init() {
+    initLanguages();
     var splitter = document.getElementById('splitter');
     var code = document.getElementById('code');
     var render = document.getElementById('render');
@@ -38,6 +39,127 @@ function init() {
 
     splitter.addEventListener('mousedown', mouseDownHandler);
 }
+
+
+function initLanguages() {
+    function createDependencyProposals(range) {
+        return [
+            {
+                label: 'MButton',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa button component",
+                insertText: '<MButton></MButton>',
+                range: range,
+            },
+            {
+                label: 'MAlert',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MAlert component",
+                insertText: '<MAlert></MAlert>',
+                range: range,
+            },
+            {
+                label: 'MResponsive',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MResponsive component",
+                insertText: '<MResponsive></MResponsive>',
+                range: range,
+            },
+            {
+                label: 'MAvatar',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MAvatar component",
+                insertText: '<MAvatar></MAvatar>',
+                range: range,
+            },
+            {
+                label: 'MBadge',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MBadge component",
+                insertText: '<MBadge></MBadge>',
+                range: range,
+            },
+            {
+                label: 'MBanner',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MBanner component",
+                insertText: '<MBanner></MBanner>',
+                range: range,
+            },
+            {
+                label: 'PBlockText ',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa PBlockText  component",
+                insertText: `<PBlockText Primary="@DateOnly.FromDateTime(DateTime.Now).ToString()"\n' +
+                    '            Secondary="@TimeOnly.FromDateTime(DateTime.Now).ToString()">\n' +
+                    '</PBlockText>`,
+                range: range,
+            },
+            {
+                label: 'MBottomNavigation',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MBottomNavigation component",
+                insertText: '<MBottomNavigation></MBottomNavigation>',
+                range: range,
+            },
+            {
+                label: 'MBottomSheet',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MBottomSheet component",
+                insertText: '<MBottomSheet></MBottomSheet>',
+                range: range,
+            },
+            {
+                label: 'MAlert',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MAlert component",
+                insertText: '<MAlert></MAlert>',
+                range: range,
+            },
+            {
+                label: 'MBreadcrumbs',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MBreadcrumbs component",
+                insertText: '<MBreadcrumbs\n' +
+                    '\tDivider="/">\n' +
+                    '</MBreadcrumbs>',
+                range: range,
+            },
+            {
+                label: 'MCard',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MCard component",
+                insertText: '<MCard Elevation="2"></MCard>',
+                range: range,
+            },
+            {
+                label: 'MChip',
+                kind: monaco.languages.CompletionItemKind.Function,
+                documentation: "masa MChip component",
+                insertText: '  <MChip></MChip>',
+                range: range,
+            }
+        ];
+    }
+
+    monaco.languages.registerCompletionItemProvider("razor", {
+        provideCompletionItems: function (model, position) {
+            var word = model.getWordUntilPosition(position);
+            var range = {
+                startLineNumber: position.lineNumber,
+                endLineNumber: position.lineNumber,
+                startColumn: word.startColumn,
+                endColumn: word.endColumn,
+            };
+            return {
+                suggestions: createDependencyProposals(range),
+            };
+        },
+    });
+
+}
+
+
 export {
     init
 }
