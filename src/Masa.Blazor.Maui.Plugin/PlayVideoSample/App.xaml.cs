2cd0b8322c8321514f28da94d2d59d6dd20dff7e
dkx4m8w <External.Hao.Guo@vwfsag.com>
2023-03-20 21:25:26 +0800

    add video sample

---
+++ src/Masa.Blazor.Maui.Plugin/PlayVideoSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayVideoSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/Masa.Blazor.Maui.Plugin/PlayVideoSample/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PlayVideoSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
