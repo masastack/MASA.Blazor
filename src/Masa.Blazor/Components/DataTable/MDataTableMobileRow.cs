80fe1963756347bfbda4f928cd5e2730d5e93bd4
capdiem <capdiem@live.com>
2023-04-19 17:27:12 +0800

    🆕 feat(DataTable): Supports for ItemClass with Func<TItem,string> (#1250)

--- src/Masa.Blazor/Components/DataTable/MDataTableMobileRow.cs
+++ src/Masa.Blazor/Components/DataTable/MDataTableMobileRow.cs
@@ -1,15 +1,17 @@
-﻿namespace Masa.Blazor;
+﻿#nullable enable
+
+namespace Masa.Blazor;
 
 public class MDataTableMobileRow<TItem> : BDataTableMobileRow<TItem>
 {
     [Parameter]
-    public Func<TItem, bool> IsSelected { get; set; }
+    public Func<TItem, bool>? IsSelected { get; set; }
 
     [Parameter]
-    public Func<TItem, bool> IsExpanded { get; set; }
+    public Func<TItem, bool>? IsExpanded { get; set; }
 
     [Parameter]
-    public string ItemClass { get; set; }
+    public Func<TItem, string?>? ItemClass { get; set; }
 
     [Parameter]
     public bool Stripe { get; set; }
@@ -23,10 +25,10 @@ public class MDataTableMobileRow<TItem> : BDataTableMobileRow<TItem>
             {
                 cssBuilder
                     .Add("m-data-table__mobile-table-row")
-                    .AddIf("m-data-table__selected", () => IsSelected(Item))
-                    .AddIf("m-data-table__expanded m-data-table__expanded__row", () => IsExpanded(Item))
+                    .AddIf("m-data-table__selected", () => IsSelected != null && IsSelected.Invoke(Item))
+                    .AddIf("m-data-table__expanded m-data-table__expanded__row", () => IsExpanded != null && IsExpanded(Item))
                     .AddIf("stripe", () => IsStripe)
-                    .Add(ItemClass);
+                    .Add(() => ItemClass?.Invoke(Item) ?? "");
             })
             .Apply("mobile-row", cssBuilder =>
             {
