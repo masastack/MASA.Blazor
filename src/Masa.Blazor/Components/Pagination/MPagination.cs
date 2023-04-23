bcfc18b212afdf859006f8933616b574e845fe03
zms9110750 <101843125+zms9110750@users.noreply.github.com>
2023-04-23 20:55:18 +0800

    修改返回逻辑，使得只返回周边的值。
    强烈建议首末页单独作为按钮`|<`和`>|`放在最外侧而不是占用按钮位置。甚至中间的`..`都占用按钮位置。导致4个以下按钮报错，5个按钮没有有效值。

--- src/Masa.Blazor/Components/Pagination/MPagination.cs
+++ src/Masa.Blazor/Components/Pagination/MPagination.cs
@@ -125,93 +125,21 @@ namespace Masa.Blazor
 
         public IEnumerable<StringNumber> GetItems()
         {
-            if (TotalVisible != null && TotalVisible.ToInt32() == 0)
-            {
-                return Enumerable.Empty<StringNumber>();
-            }
-
-            int Min(int v1, int v2, int v3)
-            {
-                var min = Math.Min(v1, v2);
-                return Math.Min(min, v3);
-            }
-
-            int Max(StringNumber v1, StringNumber v2, int v)
-            {
-                int max;
-
-                if (v1 == null || v2 == null)
-                {
-                    max = 0;
-                }
-                else
-                {
-                    max = Math.Max(v1.ToInt32(), v2.ToInt32());
-                }
-
-                //Use v to ensure max always greater than 0
-                return max == 0 ? v : max;
-            }
-
-            var maxLength = Min(
-                Max(0, TotalVisible, Length),
-                Max(0, _maxButtons, Length),
-                Length);
-
-            if (Length <= maxLength)
-            {
-                return Range(1, Length);
-            }
-
-            var items = new List<StringNumber>();
-            var even = maxLength % 2 == 0 ? 1 : 0;
-            var left = Convert.ToInt32(Math.Floor(maxLength / 2M));
-            var right = Length - left + 1 + even;
-
-            if (Value > left && Value < right)
-            {
-                var firstItem = 1;
-                var lastItem = Length;
-                var start = Value - left + 2;
-                var end = Value + left - 2 - even;
-                StringNumber secondItem = start - 1 == firstItem + 1 ? 2 : "...";
-                StringNumber beforeLastItem = end + 1 == lastItem - 1 ? end + 1 : "...";
-
-                items.Add(firstItem);
-                items.Add(secondItem);
-                items.AddRange(Range(start, end));
-                items.Add(beforeLastItem);
-                items.Add(Length);
-
-                return items;
-            }
-            else if (Value == left)
-            {
-                var end = Value + left - 1 - even;
-
-                items.AddRange(Range(1, end));
-                items.Add("...");
-                items.Add(Length);
-
-                return items;
-            }
-            else if (Value == right)
-            {
-                var start = Value - left + 1;
-                items.Add(1);
-                items.Add("...");
-                items.AddRange(Range(start, Length));
-
-                return items;
-            }
-            else
-            {
-                items.AddRange(Range(1, left));
-                items.Add("...");
-                items.AddRange(Range(right, Length));
-
-                return items;
-            }
+            var maxLength = Math.Clamp(Length, 0, Math.Min(_maxButtons, TotalVisible?.ToInt32() ?? 0));
+		    var odd = maxLength & 1;
+		    var halfRange = maxLength >> 1;
+		    if (Value < halfRange)
+		    {
+		    	return Range(1, maxLength);
+		    }
+		    else if (Value > Length - halfRange - odd)
+		    {
+		    	return Range(Length - maxLength + 1, Length);
+		    }
+		    else
+		    {
+		    	return Range(Value - halfRange + 1, Value + halfRange + odd);
+		    }
         }
 
         protected static IEnumerable<StringNumber> Range(int from, int to)
