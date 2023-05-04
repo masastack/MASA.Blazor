84ef7b5aa829395e8ef0b34731c2d2efdc320072
capdiem <capdiem@live.com>
2023-05-04 10:53:28 +0800

    📝 docs: Update masa blazor icon

--- docs/Masa.Docs.Shared/ServiceCollectionExtensions.cs
+++ docs/Masa.Docs.Shared/ServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@ namespace Masa.Docs.Shared;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddMasaDocs(this IServiceCollection services, string baseUri, string mode = BlazorMode.Server)
+    public static IMasaBlazorBuilder AddMasaDocs(this IServiceCollection services, string baseUri, string mode = BlazorMode.Server)
     {
         BlazorMode.Current = mode;
 
@@ -22,8 +22,32 @@ public static class ServiceCollectionExtensions
 
         services.AddMemoryCache();
 
+        var masaBlazorBuilder = services.AddMasaBlazor(options =>
+        {
+            options.ConfigureTheme(theme =>
+            {
+                theme.Themes.Light.Primary = "#4318FF";
+                theme.Themes.Light.Secondary = "#A18BFF";
+                theme.Themes.Light.Accent = "#005CAF";
+                theme.Themes.Light.UserDefined["Tertiary"] = "#e57373";
+            });
+            options.ConfigureIcons(IconSet.MaterialDesignIcons, aliases =>
+            {
+                aliases.UserDefined["masaBlazor"] = new SvgPath[]
+                {
+                    new(
+                        "M16 2H6V6H2V10H6V14H2V18H6V22H16C19.3137 22 22 19.3137 22 16C22 14.4633 21.4223 13.0615 20.4722 12C21.4223 10.9385 22 9.53671 22 8C22 4.68629 19.3137 2 16 2ZM6 18H16C17.1046 18 18 17.1046 18 16C18 14.8954 17.1046 14 16 14H6V18ZM6 10H16C17.1046 10 18 9.10457 18 8C18 6.89543 17.1046 6 16 6H6V10Z",
+                        new Dictionary<string, object?>()
+                        {
+                            { "fill-rule", "evenodd" },
+                            { "clip-rule", "evenodd" }
+                        })
+                };
+            });
+        });
+
         services.AddMasaBlazorDocs();
 
-        return services;
+        return masaBlazorBuilder;
     }
 }
--- docs/Masa.Docs.WebAssembly/Program.cs
+++ docs/Masa.Docs.WebAssembly/Program.cs
@@ -11,28 +11,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped<LazyAssemblyLoader>();
 
-await builder.Services.AddMasaBlazor(options =>
-{
-    options.ConfigureTheme(theme =>
-    {
-        theme.Themes.Light.Primary = "#4318FF";
-        theme.Themes.Light.Secondary = "#A18BFF";
-        theme.Themes.Light.Accent = "#005CAF";
-        theme.Themes.Light.UserDefined["Tertiary"] = "#e57373";
-    });
-    options.ConfigureIcons(IconSet.MaterialDesignIcons, aliases =>
-    {
-        aliases.UserDefined["masaBlazor"] = new SvgPath[]
-        {
-            new("M3 6.375H6.375V9.75H3V6.375Z"),
-            new("M3 13.125H6.375V16.5H3V13.125Z"),
-            new("M15.9375 3H6.375V6.375H15.9375C16.8695 6.375 17.625 7.13052 17.625 8.0625C17.625 8.99448 16.8695 9.75 15.9375 9.75H6.375V13.125H15.9375C18.7334 13.125 21 10.8584 21 8.0625C21 5.26656 18.7334 3 15.9375 3Z"),
-            new("M15.9375 9.75H6.375V13.125H15.9375C16.8695 13.125 17.625 13.8805 17.625 14.8125C17.625 15.7445 16.8695 16.5 15.9375 16.5H6.375V19.875H15.9375C18.7334 19.875 21 17.6084 21 14.8125C21 12.0166 18.7334 9.75 15.9375 9.75Z")
-        };
-    });
-}).AddI18nForWasmAsync($"{builder.HostEnvironment.BaseAddress}/_content/Masa.Docs.Shared/locale");
-
-builder.Services.AddMasaDocs(builder.HostEnvironment.BaseAddress, BlazorMode.Wasm);
+await builder.Services.AddMasaDocs(builder.HostEnvironment.BaseAddress, BlazorMode.Wasm)
+             .AddI18nForWasmAsync($"{builder.HostEnvironment.BaseAddress}/_content/Masa.Docs.Shared/locale");
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
