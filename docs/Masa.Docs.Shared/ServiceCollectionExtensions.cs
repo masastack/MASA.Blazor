b1bff381dfe75eb30d5a3cdf6b8dcc30309cda16
capdiem <capdiem@live.com>
2023-05-04 10:55:00 +0800

    simplify code

--- docs/Masa.Docs.Shared/ServiceCollectionExtensions.cs
+++ docs/Masa.Docs.Shared/ServiceCollectionExtensions.cs
@@ -33,16 +33,13 @@ public static class ServiceCollectionExtensions
             });
             options.ConfigureIcons(IconSet.MaterialDesignIcons, aliases =>
             {
-                aliases.UserDefined["masaBlazor"] = new SvgPath[]
-                {
-                    new(
-                        "M16 2H6V6H2V10H6V14H2V18H6V22H16C19.3137 22 22 19.3137 22 16C22 14.4633 21.4223 13.0615 20.4722 12C21.4223 10.9385 22 9.53671 22 8C22 4.68629 19.3137 2 16 2ZM6 18H16C17.1046 18 18 17.1046 18 16C18 14.8954 17.1046 14 16 14H6V18ZM6 10H16C17.1046 10 18 9.10457 18 8C18 6.89543 17.1046 6 16 6H6V10Z",
-                        new Dictionary<string, object?>()
-                        {
-                            { "fill-rule", "evenodd" },
-                            { "clip-rule", "evenodd" }
-                        })
-                };
+                aliases.UserDefined["masaBlazor"] = new SvgPath(
+                    "M16 2H6V6H2V10H6V14H2V18H6V22H16C19.3137 22 22 19.3137 22 16C22 14.4633 21.4223 13.0615 20.4722 12C21.4223 10.9385 22 9.53671 22 8C22 4.68629 19.3137 2 16 2ZM6 18H16C17.1046 18 18 17.1046 18 16C18 14.8954 17.1046 14 16 14H6V18ZM6 10H16C17.1046 10 18 9.10457 18 8C18 6.89543 17.1046 6 16 6H6V10Z",
+                    new Dictionary<string, object?>()
+                    {
+                        { "fill-rule", "evenodd" },
+                        { "clip-rule", "evenodd" }
+                    });
             });
         });
 
