9187c867857fc5695820fe1568fc717bb74a92db
capdiem <capdiem@live.com>
2023-05-17 19:31:46 +0800

    📝 docs: complete

--- docs/Masa.Blazor.Docs/Components/Examples/Example.razor.cs
+++ docs/Masa.Blazor.Docs/Components/Examples/Example.razor.cs
@@ -21,15 +21,12 @@ public partial class Example : NextTickComponentBase
     public bool NoActions { get; set; }
 
     [Parameter]
-    public bool Dark
-    {
-        get => _dark;
-        set => _dark = value;
-    }
+    public bool Dark { get; set; }
 
     private readonly List<(string Code, string Language)> _sections = new();
 
     private bool _rendered;
+    private bool _prevDark;
     private bool _dark;
     private bool _expand;
     private bool _showExpands;
@@ -42,6 +39,12 @@ public partial class Example : NextTickComponentBase
         base.OnParametersSet();
 
         ArgumentException.ThrowIfNullOrEmpty(File);
+
+        if (_prevDark != Dark)
+        {
+            _prevDark = Dark;
+            _dark = Dark;
+        }
     }
 
     protected override void OnInitialized()
@@ -68,6 +71,9 @@ public partial class Example : NextTickComponentBase
         {
             NextTick(async () =>
             {
+                _dark = Dark;
+                _prevDark = Dark;
+
                 if (!_rendered)
                 {
                     await Task.Delay(1);
