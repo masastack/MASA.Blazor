df769f4d319c4b64690c328987aace6df62bb5e3
capdiem <capdiem@live.com>
2023-05-30 10:22:53 +0800

    📝 docs: add annual service page

--- docs/Masa.Docs.Core/Services/AppService.cs
+++ docs/Masa.Docs.Core/Services/AppService.cs
@@ -23,29 +23,35 @@ public class AppService
     {
         var list = new List<(string Title, string URL, string? Target, string? Badge)>()
         {
-            new("docs", "/", null, null)
+            new("docs", "/", "_self", null),
         };
+
+        (string Title, string URL, string? Target, string? Badge) annualService = new("annual-service", "/annual-service", "_self", "pricing");
 
         if (project == "blazor")
         {
             list.Add(("getting-started", "/blazor/getting-started/installation", "_self", null));
             list.Add(("ui-components", "/blazor/components/all", "_self", null));
+            list.Add(annualService);
             list.Add(("pro", "https://blazor-pro.masastack.com", "_blank", "free-pro"));
             list.Add(("blog", "https://blogs.masastack.com/tags/Blazor/", "_blank", null));
             list.Add(("official-website", "https://www.masastack.com/blazor", "_blank", null));
         }
         else if (project == "framework")
         {
+            list.Add(annualService);
             list.Add(("blog", "https://blogs.masastack.com/tags/MASA-Framework/", "_blank", null));
             list.Add(("official-website", "https://www.masastack.com/framework", "_blank", null));
         }
         else if (project == "stack")
         {
+            list.Add(annualService);
             list.Add(("blog", "https://blogs.masastack.com/tags/MASA-Stack/", "_blank", null));
             list.Add(("official-website", "https://www.masastack.com/stack", "_blank", null));
         }
         else
         {
+            list.Add(annualService);
             list.Add(("blog", "https://blogs.masastack.com", "_blank", null));
             list.Add(("official-website", "https://www.masastack.com", "_blank", null));
         }
