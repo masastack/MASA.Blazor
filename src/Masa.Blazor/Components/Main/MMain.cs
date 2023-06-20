c215ab443febdcfe4d4958f4f512ffc5eaf30984
capdiem <capdiem@live.com>
2023-06-20 09:05:06 +0800

    🐛 fix(Main): The content is not rendered in some cases (#1383)

--- src/Masa.Blazor/Components/Main/MMain.cs
+++ src/Masa.Blazor/Components/Main/MMain.cs
@@ -5,7 +5,7 @@ namespace Masa.Blazor;
 public class MMain : BMain
 {
     [Inject]
-    public MasaBlazor? MasaBlazor { get; set; }
+    public MasaBlazor MasaBlazor { get; set; } = null!;
 
     private readonly string[] _applicationProperties = new string[]
     {
@@ -21,7 +21,7 @@ public class MMain : BMain
     {
         base.OnInitialized();
 
-        MasaBlazor!.Application.PropertyChanged += OnApplicationPropertyChanged;
+        MasaBlazor.Application.PropertyChanged += OnApplicationPropertyChanged;
     }
 
     private void OnApplicationPropertyChanged(object? sender, PropertyChangedEventArgs e)
@@ -38,7 +38,7 @@ public class MMain : BMain
             .Apply(cssBuilder => { cssBuilder.Add("m-main"); }, styleBuilder =>
             {
                 styleBuilder
-                    .Add($"padding-top:{MasaBlazor!.Application.Top + MasaBlazor.Application.Bar}px")
+                    .Add($"padding-top:{MasaBlazor.Application.Top + MasaBlazor.Application.Bar}px")
                     .Add($"padding-right:{MasaBlazor.Application.Right}px")
                     .Add($"padding-bottom:{MasaBlazor.Application.Footer + MasaBlazor.Application.InsetFooter + MasaBlazor.Application.Bottom}px")
                     .Add($"padding-left:{MasaBlazor.Application.Left}px");
