dfd7bfdd451773da1d71b78413e6fe4d1c08f424
token <239573049@qq.com>
2023-07-29 02:46:47 +0800

    支持组件预览编辑 动态更新UI预览

---
+++ docs/Masa.Blazor.Docs/Models/Sections.cs
@@ -0,0 +1,16 @@
+﻿namespace Masa.Blazor.Docs;
+
+public class Sections
+{
+    public string Code { get; set; }
+
+    public string Language { get; set; }
+
+    public MMonacoEditor MonacoEditor { get; set; }
+
+    public Sections(string code, string language)
+    {
+        Code = code;
+        Language = language;
+    }
+}
