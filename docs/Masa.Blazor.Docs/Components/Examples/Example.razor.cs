f18cdc9c009ffa669ccfbe7f335b5b39045e1120
token <61819790+239573049@users.noreply.github.com>
2023-08-03 20:48:38 +0800

    docs: support for online editing of demos (#1455)
    * 支持组件预览编辑
    动态更新UI预览
    
    * 修改后缀名
    
    * Update docs/Masa.Blazor.Docs/Components/Examples/Example.razor.cs
    
    Co-authored-by: capdiem <capdiem@live.com>
    
    * add Copy Code
    add Restore Code
    
    * 增加Server编译支持
    
    * 支持Monaco 跟随主题
    
    ---------
    
    Co-authored-by: capdiem <capdiem@live.com>

--- docs/Masa.Blazor.Docs/Components/Examples/Example.razor.cs
+++ docs/Masa.Blazor.Docs/Components/Examples/Example.razor.cs
@@ -1,38 +1,76 @@
-﻿using System.Reflection;
+﻿using Masa.Blazor.Docs.Examples.components.selects;
+using Masa.Blazor.Extensions.Languages.Razor;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.CodeAnalysis;
+using Microsoft.JSInterop;
+using System.Reflection;
 
 namespace Masa.Blazor.Docs.Components;
 
 [JSCustomElement("masa-example")]
 public partial class Example : NextTickComponentBase
 {
-    [Inject]
-    private BlazorDocService DocService { get; set; } = null!;
+    [Inject] private BlazorDocService DocService { get; set; } = null!;
 
-    [Inject]
-    private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] public IHttpClientFactory HttpClientFactory { get; set; } = null!;
 
-    [Parameter, EditorRequired]
-    public string File { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
 
-    [Parameter]
-    public int Index { get; set; }
+    [Inject] private IPopupService PopupService { get; set; } = null!;
+    [Inject] public IJSRuntime Js { get; set; } = null!;
+    [Inject] public MasaBlazor MasaBlazor { get; set; }
 
-    [Parameter]
-    public bool NoActions { get; set; }
+    [Parameter, EditorRequired] public string File { get; set; } = null!;
 
-    [Parameter]
-    public bool Dark { get; set; }
+    [Parameter] public int Index { get; set; }
 
-    private readonly List<(string Code, string Language)> _sections = new();
+    [Parameter] public bool NoActions { get; set; }
+
+    [Parameter] public bool Dark { get; set; }
+
+    private Sections _sections;
+
+    /// <summary>
+    /// 编译器需要使用的程序集
+    /// </summary>
+    private static readonly List<string> assemblies = new()
+    {
+        "BlazorComponent",
+        "Masa.Blazor",
+        "OneOf",
+        "FluentValidation",
+        "netstandard",
+        "FluentValidation.DependencyInjectionExtensions",
+        "System",
+        "Microsoft.AspNetCore.Components",
+        "System.Linq.Expressions",
+        "System.Net.Http.Json",
+        "System.Private.CoreLib",
+        "Microsoft.AspNetCore.Components.Web",
+        "System.Collections",
+        "System.Linq",
+        "System.Runtime"
+    };
 
     private bool _rendered;
     private bool _prevDark;
     private bool _dark;
     private bool _expand;
     private bool _showExpands;
+    private bool _load;
+    private static bool _initialize;
     private StringNumber _selected = 0;
     private Type? _type;
+    private DotNetObjectReference<Example>? _objRef;
+    private MMonacoEditor MonacoEditor;
     private List<(string Icon, string Path, Action? OnClick, string? href)> _tooltips = new();
+
+    private object options = new
+    {
+        language = "html",
+        theme = "vs",
+        automaticLayout = true,
+    };
 
     protected override void OnParametersSet()
     {
@@ -51,11 +89,19 @@ public partial class Example : NextTickComponentBase
     {
         base.OnInitialized();
 
-        var githubUri = $"https://github.com/masastack/MASA.Blazor/blob/main/docs/Masa.Blazor.Docs/{File.Replace(".", "/").Replace("_", "-")}.razor";
+        _objRef = DotNetObjectReference.Create(this);
+
+        CompileRazorProjectFileSystem.AddGlobalUsing("@using BlazorComponent");
+        CompileRazorProjectFileSystem.AddGlobalUsing("@using Masa.Blazor");
+        CompileRazorProjectFileSystem.AddGlobalUsing("@using Masa.Blazor.Presets");
+
+        var githubUri =
+            $"https://github.com/masastack/MASA.Blazor/blob/main/docs/Masa.Blazor.Docs/{File.Replace(".", "/").Replace("_", "-")}.razor";
 
         // From File: Examples.components.alerts.Border
         // To Path: pages/Examples/components/alerts/examples/Border.txt
-        var sections = File.Replace("Examples", "pages", StringComparison.OrdinalIgnoreCase).Replace("_", "-").Split(".").ToList();
+        var sections = File.Replace("Examples", "pages", StringComparison.OrdinalIgnoreCase).Replace("_", "-")
+            .Split(".").ToList();
         sections.Insert(sections.Count - 1, "examples");
         var sourceCodePath = string.Join("/", sections) + ".txt";
         var sourceCodeUri = $"https://docs.masastack.com/_content/Masa.Blazor.Docs/{sourceCodePath}";
@@ -67,6 +113,23 @@ public partial class Example : NextTickComponentBase
             new("mdi-github", "view-in-github", null, githubUri),
             new("mdi-code-tags", "view-source", ToggleCode, null)
         };
+
+        MasaBlazor.OnThemeChange += async (theme) =>
+        {
+            if (theme.Dark && MonacoEditor !=null)
+            {
+                await MonacoEditor?.SetThemeAsync("vs-dark");
+            }
+            else if(MonacoEditor != null)
+            {
+                await MonacoEditor?.SetThemeAsync("vs");
+            }
+        };
+    }
+
+    private async Task InitCompleteHandle()
+    {
+        await MonacoEditor.AddCommandAsync(2097, _objRef, nameof(RunCode));
     }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
@@ -101,28 +164,48 @@ public partial class Example : NextTickComponentBase
 
                     var sourceCode = await DocService.ReadExampleAsync(category, title, _type.Name);
 
-                    var (razor, cs, css) = ResolveSourceCode(sourceCode);
-
-                    if (!string.IsNullOrWhiteSpace(razor))
-                    {
-                        _sections.Add((razor, nameof(razor)));
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(cs))
-                    {
-                        _sections.Add((cs, nameof(cs)));
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(css))
-                    {
-                        _sections.Add((css, nameof(css)));
-                    }
+                    _sections = new Sections(sourceCode, "razor");
 
                     StateHasChanged();
                 }
             });
 
             StateHasChanged();
+        }
+    }
+
+    private async Task RestoreCode()
+    {
+        var segments = NavigationManager.GetSegments();
+
+        var category = segments[2].TrimEnd('/');
+        var title = segments[3].TrimEnd('/');
+
+        var executingAssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+        _type = Type.GetType($"{executingAssemblyName}.{File}");
+
+        if (_type == null)
+        {
+            StateHasChanged();
+            return;
+        }
+
+        var sourceCode = await DocService.ReadExampleAsync(category, title, _type.Name);
+        await MonacoEditor.SetValueAsync(sourceCode);
+    }
+
+    private async Task CopyCode()
+    {
+        var code = await MonacoEditor.GetValueAsync();
+
+        if (!string.IsNullOrWhiteSpace(code))
+        {
+            await Js.InvokeVoidAsync(JsInteropConstants.CopyText, code);
+            await PopupService.EnqueueSnackbarAsync("Copy success", AlertTypes.Success);
+        }
+        else
+        {
+            await PopupService.EnqueueSnackbarAsync("The code is empty", AlertTypes.Success);
         }
     }
 
@@ -141,35 +224,85 @@ public partial class Example : NextTickComponentBase
         }
     }
 
-    private static(string? Razor, string? cs, string? css) ResolveSourceCode(string sourceCode)
+    [JSInvokable(nameof(RunCode))]
+    public async Task RunCode()
     {
-        string? razor = null;
-        string? cs = null;
-        string? css = null;
+        _load = true;
+        await Task.Delay(50);
+        _ = InvokeAsync(StateHasChanged);
 
-        var code = sourceCode;
-        var cssFrom = sourceCode.IndexOf("<style", StringComparison.Ordinal);
-        var cssTo = sourceCode.IndexOf("</style>", StringComparison.Ordinal) + "</style>".Length;
-
-        if (cssFrom > -1 && cssTo > -1)
+        if (!_initialize)
         {
-            var cssContent = sourceCode.Substring(cssFrom, cssTo - cssFrom);
-            css = cssContent;
-
-            code = code.Replace(cssContent, "");
+            RazorCompile.Initialized(await GetReference(), GetRazorExtension());
         }
 
-        var codeIndex = code.IndexOf("@code");
-        if (codeIndex > -1)
+        try
         {
-            razor = code.Substring(0, codeIndex).Trim();
-            cs = code.Substring(codeIndex).Trim();
+
+            string code = await MonacoEditor.GetValueAsync();
+            _type = RazorCompile.CompileToType(new CompileRazorOptions()
+            {
+                OptimizationLevel = OptimizationLevel.Release,
+                Code = code,
+            });
+        }
+        catch (Exception e)
+        {
+            await PopupService.EnqueueSnackbarAsync(e.Message, AlertTypes.Error);
+        }
+
+        _load = false;
+        _ = InvokeAsync(StateHasChanged);
+    }
+
+
+    private async Task<List<PortableExecutableReference>?> GetReference()
+    {
+        var portableExecutableReferences = new List<PortableExecutableReference>();
+        var iswebAssembly = Js is IJSInProcessRuntime;
+        if (iswebAssembly)
+        {
+            using var http = HttpClientFactory.CreateClient("masa-docs");
+            foreach (var asm in assemblies)
+            {
+                try
+                {
+                    await using var stream = await http!.GetStreamAsync($"_framework/{asm}.dll");
+                    if (stream.Length > 0)
+                    {
+                        portableExecutableReferences?.Add(MetadataReference.CreateFromStream(stream));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
         else
         {
-            razor = code.Trim();
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                try
+                {
+                    portableExecutableReferences?.Add(MetadataReference.CreateFromFile(assembly.Location));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
         }
 
-        return (razor, cs, css);
+        _initialize = true;
+
+        return portableExecutableReferences;
+    }
+
+    private static List<RazorExtension> GetRazorExtension()
+    {
+        return typeof(Example).Assembly.GetReferencedAssemblies()
+            .Select(asm => new AssemblyExtension(asm.FullName, AppDomain.CurrentDomain.Load(asm.FullName)))
+            .Cast<RazorExtension>().ToList();
     }
 }
