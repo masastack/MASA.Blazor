fdff0e62ef72773bc3fbc4399484c5c5392c3d21
capdiem <capdiem@live.com>
2023-08-18 10:11:26 +0800

    docs: API generator could resolve obsolete parameter and render to API table (#1493)
    * 📝 docs(api): Support for Obsolete attribute
    
    * 📝 docs: use no transition click  example and api
    
    * 📝 docs: TOC i18n error

--- src/Masa.Blazor.Docs.ApiGenerator/ParameterInfo.cs
+++ src/Masa.Blazor.Docs.ApiGenerator/ParameterInfo.cs
@@ -2,18 +2,19 @@
 
 internal class ParameterInfo
 {
-    public ParameterInfo(string name, string type, string? typeDesc = null, string? defaultValue = null)
+    public ParameterInfo(string name, string type, string? typeDesc = null, string? defaultValue = null, bool isObsolete = false)
     {
         Name = name;
         Type = type;
         TypeDesc = typeDesc;
         DefaultValue = defaultValue;
+        IsObsolete = isObsolete;
     }
-    
+
     public string Name { get; set; } = null!;
 
     public string Type { get; set; } = null!;
-    
+
     public string? TypeDesc { get; set; }
 
     public string? DefaultValue { get; set; }
@@ -21,4 +22,6 @@ internal class ParameterInfo
     public string? Description { get; set; }
 
     public bool Required { get; set; }
+
+    public bool IsObsolete { get; set; }
 }
