a15e1e735019d911ea4e2b6034fcba96d0e3cc3e
capdiem <capdiem@live.com>
2023-09-08 18:02:31 +0800

    🚧 feat: wip

---
+++ src/Masa.Blazor/Components/Lazy/MLazy.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace Masa.Blazor;
+
+public partial class MLazy
+{
+    [Inject] private DomEventJsInterop DomEventJsInterop { get; set; } = null!;
+    
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    [Parameter] public StringNumber? MinHeight { get; set; }
+
+    private Block Block => new("m-lazy");
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        
+        await DomEventJsInterop.IntersectionObserver(Ref.GetSelector(), TryAutoFocus, OnResize);
+
+    }
+}
