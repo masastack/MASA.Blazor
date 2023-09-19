a9849ec97cec2c12b3e5761777b8b247b578e744
capdiem <capdiem@live.com>
2023-09-19 17:40:15 +0800

    ⚰ refactor: remove unused code

--- src/Masa.Blazor/Components/Swiper/MSwiperSlide.razor.cs
+++ src/Masa.Blazor/Components/Swiper/MSwiperSlide.razor.cs
@@ -7,8 +7,6 @@ public partial class MSwiperSlide
     [Parameter] public string? Style { get; set; }
 
     [Parameter] public RenderFragment? ChildContent { get; set; }
-
-    [Parameter] public string? Value { get; set; }
     
     private Block Block => new("m-swiper-slide");
 }
