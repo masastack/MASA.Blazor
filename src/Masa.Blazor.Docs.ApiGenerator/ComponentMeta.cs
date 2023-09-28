05bbf6be3ebf5accf079a50b55e7ae64e13950c1
capdiem <capdiem@live.com>
2023-09-29 03:11:01 +0800

    build error

---
+++ src/Masa.Blazor.Docs.ApiGenerator/ComponentMeta.cs
@@ -0,0 +1,21 @@
+﻿namespace Masa.Blazor.Docs.ApiGenerator;
+
+public class ComponentMeta
+{
+    public ComponentMeta(string name, Dictionary<string, List<ParameterInfo>> parameters)
+    {
+        Name = name;
+        Parameters = parameters;
+    }
+
+    public ComponentMeta(string name, Dictionary<string, List<ParameterInfo>> parameters, string namespaceName):this(name, parameters)
+    {
+        NamespaceName = namespaceName;
+    }
+
+    public string Name { get; private set; }
+
+    public Dictionary<string, List<ParameterInfo>> Parameters { get; private set; }
+    
+    public string? NamespaceName { get; set; }
+}
