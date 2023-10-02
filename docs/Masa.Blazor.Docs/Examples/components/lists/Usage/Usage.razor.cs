e7f2c74344ddafc4c7a9afb28f82a5bf8038633e
capdiem <capdiem@live.com>
2023-10-02 12:17:44 +0800

    📝 docs: Clean up examples of usage

---
+++ docs/Masa.Blazor.Docs/Examples/components/lists/Usage/Usage.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace Masa.Blazor.Docs.Examples.components.lists;
+
+public class Usage : Masa.Blazor.Docs.Components.Usage
+{
+    protected override ParameterList<bool> GenToggleParameters() => new()
+    {
+        { nameof(AdvanceUsage.TwoLine), false },
+        { nameof(AdvanceUsage.ThreeLine), false },
+    };
+
+    protected override ParameterList<CheckboxParameter> GenCheckboxParameters() => new()
+    {
+        { nameof(AdvanceUsage.ShowAvatar), new CheckboxParameter() },
+        { nameof(AdvanceUsage.Dense), new CheckboxParameter() }
+    };
+
+    public Usage() : base(typeof(AdvanceUsage))
+    {
+    }
+
+    protected override string ComponentName => nameof(MList);
+
+    protected override IEnumerable<ParameterItem<CheckboxParameter>> ActiveCheckboxParameters
+        => base.ActiveCheckboxParameters.Where(u => u.Key != nameof(AdvanceUsage.ShowAvatar));
+}
