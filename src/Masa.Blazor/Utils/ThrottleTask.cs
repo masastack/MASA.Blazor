d1bfd2349fa9425991852cd1a40e4e26abc0736a
capdiem <capdiem@live.com>
2023-11-15 13:08:35 +0800

    ⚡ refactor(PullRefresh): Improve the performance (#1643)
    * ⚡ refactor(PullRefresh): Improve the performance
    
    * remove unnecessary code

---
+++ src/Masa.Blazor/Utils/ThrottleTask.cs
@@ -0,0 +1,67 @@
+﻿namespace Masa.Blazor.Utils;
+
+public class ThrottleTask
+{
+    private readonly object _lock = new();
+
+    private Task _currentTask = Task.CompletedTask;
+    private bool _canceled;
+
+    public ThrottleTask() : this(300)
+    {
+    }
+
+    public ThrottleTask(int interval)
+    {
+        Interval = interval;
+    }
+
+    public int Interval { get; }
+
+    public async Task RunAsync(Func<Task> task)
+    {
+        lock (_lock)
+        {
+            if (!_currentTask.IsCompleted)
+            {
+                return; // ignore if the previous task is not completed
+            }
+
+            _currentTask = Task.Delay(Interval);
+            _canceled = false;
+        }
+
+        try
+        {
+            await _currentTask;
+
+            if (_canceled)
+            {
+                return;
+            }
+
+            await task.Invoke();
+        }
+        catch (TaskCanceledException)
+        {
+            // ignored
+        }
+    }
+
+    public Task RunAsync(Action task)
+        => RunAsync(() =>
+        {
+            task();
+            return Task.CompletedTask;
+        });
+
+    /// <summary>
+    /// Cancel the task if it's running.
+    /// It's useful when you have got your result
+    /// and don't want the rest of the task to be executed.
+    /// </summary>
+    public void Cancel()
+    {
+        _canceled = true;
+    }
+}
