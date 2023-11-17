eb7d4655f69e060858be7f8f8eb275b8dddee0ff
capdiem <capdiem@live.com>
2023-11-17 14:16:18 +0800

    🐛 fix(BaiduMap): containerId may be removed if component was disposed (#1648)

--- src/Masa.Blazor/Components/Map/BaiduMap/BaiduMapJSModule.cs
+++ src/Masa.Blazor/Components/Map/BaiduMap/BaiduMapJSModule.cs
@@ -6,11 +6,26 @@
         {
         }
 
-        public async ValueTask<IBaiduMapJSObjectReferenceProxy> InitAsync(
+        public async ValueTask<IBaiduMapJSObjectReferenceProxy?> InitAsync(
             string containerId,
             BaiduMapInitOptions options,
             IBaiduMapJsCallbacks owner)
-            => new BaiduMapJSObjectReferenceProxy(await InvokeAsync<IJSObjectReference>("init", containerId, options), owner);
+        {
+            try
+            {
+                var jsObjectReference = await InvokeAsync<IJSObjectReference>("init", containerId, options);
+                return new BaiduMapJSObjectReferenceProxy(jsObjectReference, owner);
+            }
+            catch (JSException e)
+            {
+                // TODO: after https://github.com/dotnet/aspnetcore/issues/52070 is fixed, remove this
+                if (e.Message.StartsWith("Cannot create a JSObjectReference from the"))
+                {
+                    return null;
+                }
 
+                throw;
+            }
+        }
     }
 }
