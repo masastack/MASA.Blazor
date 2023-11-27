11600525fe914aba3b006409f8e9c4288cdf88ab
capdiem <capdiem@live.com>
2023-11-27 11:53:10 +0800

    update

--- src/Masa.Blazor/wwwroot/js/ssr.js
+++ src/Masa.Blazor/wwwroot/js/ssr.js
@@ -7,18 +7,9 @@ export function onLoad() {
 // the initial load.
 export function onUpdate() {
     console.log('Update');
-
-    them();
-    
-    
 }
 
 // Called when an enhanced page update removes the script from the page.
 export function onDispose() {
     console.log('Dispose');
-}
-
-function them() {
-    console.log('initTheme: change the theme css of .m-application')
-    BlazorComponent.interop.ssr.initTheme()
 }
