9d9c29e49d61953c8303e369fc120b4ce2623fea
capdiem <capdiem@live.com>
2023-12-04 16:37:07 +0800

    move

--- src/Masa.Blazor/Components/SsrStateProvider/MSsrStateProvider.cs
+++ src/Masa.Blazor/Components/SsrStateProvider/MSsrStateProvider.cs
@@ -1,40 +1,6 @@
 ﻿using System.ComponentModel;
-using Microsoft.AspNetCore.Components.Routing;
 
 namespace Masa.Blazor;
-
-public class MasaBlazorSsrState
-{
-    public string? Culture { get; set; }
-
-    public PassiveState Passive { get; set; }
-
-    public bool? Rtl { get; set; }
-
-    public bool? Dark { get; set; }
-
-    public class PassiveState
-    {
-        public ApplicationState Application { get; set; }
-
-        public class ApplicationState
-        {
-            public double Bar { get; set; }
-
-            public double Top { get; set; }
-
-            public double Right { get; set; }
-
-            public double Bottom { get; set; }
-
-            public double Left { get; set; }
-
-            public double Footer { get; set; }
-
-            public double InsetFooter { get; set; }
-        }
-    }
-}
 
 public class MSsrStateProvider : ComponentBase, IDisposable
 {
@@ -105,7 +71,6 @@ public class MSsrStateProvider : ComponentBase, IDisposable
         try
         {
             await Task.Delay(300, _cancellationTokenSource.Token);
-            Console.Out.WriteLine("[MSsrAppProvider] SaveStateAsync");
             _ = JSRuntime.InvokeVoidAsync(JsInteropConstants.SsrUpdatePassiveState,  new MasaBlazorSsrState.PassiveState()
             {
                 Application = new MasaBlazorSsrState.PassiveState.ApplicationState()
