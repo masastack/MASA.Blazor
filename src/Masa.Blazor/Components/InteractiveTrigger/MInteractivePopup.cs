113dfc74850a3d7f2b7d349e545c24ec660c4fcf
capdiem <capdiem@live.com>
2023-12-13 17:01:16 +0800

    🆕 feat: Add MInteractiveTrigger(s) and MInteractivePopup components

---
+++ src/Masa.Blazor/Components/InteractiveTrigger/MInteractivePopup.cs
@@ -0,0 +1,52 @@
+﻿namespace Masa.Blazor;
+
+/// <summary>
+/// A abstract class for components that use the <see cref="MInteractiveTrigger{TValue}"/>
+/// or <see cref="MInteractiveTriggers{TValue}"/> component as a trigger.
+/// </summary>
+public abstract class MInteractivePopup : ComponentBase, IOutsideClickJsCallback, IAsyncDisposable
+{
+    [Inject] private OutsideClickJSModule OutsideClickJSModule { get; set; } = null!;
+
+    [Inject] protected NavigationManager NavigationManager { get; set; } = null!;
+
+    /// <summary>
+    /// The query name of url for trigger a interactive popup. 
+    /// </summary>
+    [Parameter] public string QueryName { get; set; } = null!;
+
+    /// <summary>
+    /// The activator selector.
+    /// </summary>
+    [Parameter] public string Activator { get; set; } = null!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await OutsideClickJSModule.InitializeAsync(this, Activator, ".m-interactive-trigger__popup");
+        }
+    }
+
+    public async Task HandleOnOutsideClickAsync()
+    {
+        // TODO: https://github.com/dotnet/aspnetcore/issues/52705
+        await Task.Delay(100); 
+        NavigationManager.NavigateWithQueryParameter(QueryName, null);
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        try
+        {
+            await DisposeAsync();
+            await OutsideClickJSModule.UnbindAndDisposeAsync();
+        }
+        catch (JSDisconnectedException)
+        {
+            // ignore
+        }
+    }
+
+    protected virtual Task DisposeAsync() => Task.CompletedTask;
+}
