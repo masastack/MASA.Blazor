5f4f61c8c6926de5872250174ee71b4733b5aba6
capdiem <capdiem@live.com>
2024-01-05 11:01:46 +0800

    ♻ refactor(datatable): calculate real width of col on colgroup item

---
+++ src/Masa.Blazor/Components/DataTable/MDataTableColgroup.cs
@@ -0,0 +1,17 @@
+﻿namespace Masa.Blazor;
+
+public class MDataTableColgroup<TItem> : BDataTableColgroup<TItem>
+{
+    [Inject] private MasaBlazor MasaBlazor { get; set; } = null!;
+
+    protected override void SetComponentCss()
+    {
+        base.SetComponentCss();
+
+        CssProvider.Apply("col", css =>
+        {
+            var header = css.Data as DataTableHeader;
+            css.AddIf("divider", () => header?.Divider is true);
+        });
+    }
+}
