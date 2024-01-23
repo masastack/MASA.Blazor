74d0fc21ba870e4c9c7db8d35666f5681f96c659
capdiem <capdiem@live.com>
2024-01-23 11:49:10 +0800

    📝 docs: add notifications menu (#1731)

---
+++ docs/Masa.Docs.Shared/Models/UserStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace Masa.Docs.Shared.Models;
+
+public class UserStorage
+{
+    public Notifications Notifications { get; set; } = new();
+}
+
+public class Notifications
+{
+    public List<string> Read { get; set; } = new();
+}
