5e7a13d0e161daeb03beadd05e54895c303f6092
capdiem <capdiem@live.com>
2024-01-27 16:25:45 +0800

    complete

--- docs/Masa.Docs.Shared/Pages/Home.razor.cs
+++ docs/Masa.Docs.Shared/Pages/Home.razor.cs
@@ -2,60 +2,48 @@
 
 public partial class Home : ComponentBase
 {
+    [CascadingParameter(Name = "Culture")]
+    private string? Culture { get; set; }
+
     [Parameter]
     [SupplyParameterFromQuery]
     public string Product { get; set; } = "stack";
+
+    private bool _prevXs;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        MasaBlazor.BreakpointChanged += MasaBlazorOnBreakpointChanged;
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _prevXs = MasaBlazor.Breakpoint.Xs;
+        }
+    }
+
+    private void MasaBlazorOnBreakpointChanged(object? sender, BreakpointChangedEventArgs e)
+    {
+        if (_prevXs != MasaBlazor.Breakpoint.Xs)
+        {
+            _prevXs = MasaBlazor.Breakpoint.Xs;
+            InvokeAsync(StateHasChanged);
+        }
+    }
 
     protected override void OnParametersSet()
     {
         base.OnParametersSet();
 
-        Product ??= "stack";
+        Product ??= Culture == "zh-CN" ? "stack" : "blazor";
     }
 
-    private string _blazorGettingStartedIntro =
-        """
-        To get started quickly, you can use the MASA.Template template to quickly create a project.
-        ```bash
-        dotnet new install MASA.Template
-        ```
-        ```bash
-        dotnet new blazor -o BlazorApp -ai
-        ```
-        """;
-
-    private string _blazorIconIntro =
-        """
-        Out of the box, MASA Blazor supports 5 popular icon font libraries:
-        [Material Design Icons](https://materialdesignicons.com/),
-        [Material Icons](https://fonts.google.com/icons),
-        [Font Awesome 4](https://fontawesome.com/v4/icons/),
-        [Font Awesome 5](https://fontawesome.com/v5/search?m=free)
-        and [Font Awesome 6](https://fontawesome.com/v6/search?o=r&m=free).
-        """;
-
-    private string _blazorThemeIntro =
-        """
-        轻松地通过编程的方式改变应用程序的颜色。重新构建默认样式表并根据您的特定需求自定义框架的各个方面。
-        在 Material Design 2 颜色规则的基础上，引入了 Material Design 3 中 `On[Rule]` 的规则的支持。
-        """;
-
-    private string _blazorUtilityClassesIntro =
-        """
-        MASA Blazor provides a set of utility classes that can be used to quickly build a UI,
-        including: [Border radius](/blazor/styles-and-animations/border-radius),
-        [Colors](/blazor/styles-and-animations/color), 
-        [Display helpers](/blazor/styles-and-animations/display-helpers),
-        [Elevation](/blazor/styles-and-animations/elevation),
-        [Flex](/blazor/styles-and-animations/flex),
-        [Float](/blazor/styles-and-animations/floats),
-        [Overflow](/blazor/styles-and-animations/overflow),
-        [Spacing](/blazor/styles-and-animations/spacing),
-        [Text and typography](/blazor/styles-and-animations/text-and-typography).
-        """;
-
-    private string _blazorComponentsIntro =
-        """
-        MASA Blazor 提供基础组件、预置组件、JS模块和实验性组件。
-        """;
+    public void Dispose()
+    {
+        MasaBlazor.BreakpointChanged -= MasaBlazorOnBreakpointChanged;
+    }
 }
