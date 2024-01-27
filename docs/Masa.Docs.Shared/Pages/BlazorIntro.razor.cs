1a1357a0ae1b8cecc795692c302826738bd70157
capdiem <capdiem@live.com>
2024-01-27 16:33:33 +0800

    docs: redesign the home page (#1739)
    * 🚧 feat: wip
    
    * 🚧 feat: wip
    
    * complete
    
    * update

---
+++ docs/Masa.Docs.Shared/Pages/BlazorIntro.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Masa.Docs.Shared.Pages;
+
+public partial class BlazorIntro
+{
+    private string? Name { get; set; }
+
+    private string? Password { get; set; }
+
+    private MForm? _form;
+    private bool _loading;
+
+    private static CultureInfo zhCn = new("zh-CN");
+    private static CultureInfo enUs = new("en-US");
+
+    string Culture => I18n.Culture.Equals(zhCn) ? "CN" : "EN";
+
+    private void CultureChange(string val)
+    {
+        I18n.SetCulture(val == "CN" ? zhCn : enUs);
+    }
+
+    private async Task Commit()
+    {
+        _loading = true;
+        StateHasChanged();
+
+        if (_form!.Validate())
+        {
+            await Task.Delay(1000);
+            await PopupService.EnqueueSnackbarAsync("Commit success", AlertTypes.Success);
+        }
+
+        _loading = false;
+    }
+}
