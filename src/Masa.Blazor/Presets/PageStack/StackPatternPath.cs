98b33a7f493bacd1af55fa90a2a9756a5e57bb53
capdiem <capdiem@live.com>
2024-03-16 16:55:38 +0800

    🆕 feat: add PageStack component

---
+++ src/Masa.Blazor/Presets/PageStack/StackPatternPath.cs
@@ -0,0 +1,16 @@
+﻿namespace Masa.Blazor.Presets;
+
+public class StackPatternPath : PatternPath
+{
+    public StackPatternPath(string absolutePath) : base(absolutePath)
+    {
+        Active = true;
+    }
+
+    public StackPatternPath(string pattern, string absolutePath) : base(pattern, absolutePath)
+    {
+        Active = true;
+    }
+
+    public bool Active { get; set; }
+}
