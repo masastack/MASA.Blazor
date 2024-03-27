77c68bad1c2cbca0afcbe0c680a9f8501706cf3a
capdiem <capdiem@live.com>
2024-03-27 13:55:50 +0800

    🆕 feat: support for setting the lifetime of MasaBlazor service

--- src/Masa.Blazor/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Masa.Blazor/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -9,30 +9,46 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    public static IMasaBlazorBuilder AddMasaBlazor(this IServiceCollection services)
+    /// <summary>
+    /// Add common services required by MASA Blazor components.
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="masaBlazorServiceLifetime">The service lifetime of <see cref="MasaBlazor"/> service.</param>
+    public static IMasaBlazorBuilder AddMasaBlazor(this IServiceCollection services,
+        ServiceLifetime masaBlazorServiceLifetime = ServiceLifetime.Scoped)
     {
         services.AddBlazorComponent();
-        return services.AddMasaBlazorInternal();
+        return services.AddMasaBlazorInternal(masaBlazorServiceLifetime: masaBlazorServiceLifetime);
     }
 
-    public static IMasaBlazorBuilder AddMasaBlazor(this IServiceCollection services, Action<MasaBlazorOptions> optionsAction)
+    /// <summary>
+    /// Add common services required by MASA Blazor components.
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="optionsAction">The action to configure the options.</param>
+    /// <param name="masaBlazorServiceLifetime">The service lifetime of <see cref="MasaBlazor"/> service.</param>
+    public static IMasaBlazorBuilder AddMasaBlazor(this IServiceCollection services,
+        Action<MasaBlazorOptions> optionsAction,
+        ServiceLifetime masaBlazorServiceLifetime = ServiceLifetime.Scoped)
     {
         var options = new MasaBlazorOptions();
         optionsAction.Invoke(options);
 
         services.AddBlazorComponent(o => { o.Locale = options.Locale; });
-        return services.AddMasaBlazorInternal(optionsAction);
+        return services.AddMasaBlazorInternal(optionsAction, masaBlazorServiceLifetime);
     }
 
-    private static IMasaBlazorBuilder AddMasaBlazorInternal(this IServiceCollection services, Action<MasaBlazorOptions>? optionsAction = null)
+    private static IMasaBlazorBuilder AddMasaBlazorInternal(this IServiceCollection services,
+        Action<MasaBlazorOptions>? optionsAction = null,
+        ServiceLifetime masaBlazorServiceLifetime = ServiceLifetime.Scoped)
     {
-        services.TryAddScoped<Application>();
-        services.TryAddScoped(serviceProvider =>
+        services.TryAdd<Application>(masaBlazorServiceLifetime);
+        services.TryAdd(ServiceDescriptor.Describe(typeof(MasaBlazor), sp =>
         {
             var options = new MasaBlazorOptions();
             optionsAction?.Invoke(options);
 
-            var application = serviceProvider.GetRequiredService<Application>();
+            var application = sp.GetRequiredService<Application>();
             return new MasaBlazor(
                 options.RTL,
                 options.Breakpoint,
@@ -41,13 +57,13 @@ public static class ServiceCollectionExtensions
                 options.Icons,
                 options.SSR,
                 options.Defaults);
-        });
+        }, masaBlazorServiceLifetime));
 
-        services.TryAddScoped<IPopupService, PopupService>();
+        services.TryAdd<IPopupService, PopupService>(masaBlazorServiceLifetime);
+
         services.TryAddScoped<IErrorHandler, MErrorHandler>();
         services.AddSingleton<IAbstractComponentTypeMapper, MasaBlazorComponentTypeMapper>();
         services.TryAddSingleton<InternalHttpClient>();
-
         services.TryAddScoped<EChartsJSModule>();
         services.TryAddScoped<MonacoEditorJSModule>();
         services.TryAddScoped<MarkdownItJSModule>();
@@ -62,5 +78,18 @@ public static class ServiceCollectionExtensions
         services.TryAddScoped<SortableJSModule>();
 
         return new MasaBlazorBuilder(services);
+    }
+
+    private static void TryAdd<TService, TImplementation>(this IServiceCollection services, ServiceLifetime lifetime)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        services.TryAdd(new ServiceDescriptor(typeof(TService), typeof(TImplementation), lifetime));
+    }
+
+    private static void TryAdd<TService>(this IServiceCollection services, ServiceLifetime lifetime)
+        where TService : class
+    {
+        services.TryAdd(new ServiceDescriptor(typeof(TService), typeof(TService), lifetime));
     }
 }
