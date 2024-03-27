86f6f1f2540e03727075a767ff7f08aa936209aa
pankey.chen <jianming.chen@zongmutech.com>
2024-03-27 22:38:20 +0800

    feat: Add closeable option to TabOptions.

--- src/Masa.Blazor/Presets/PageTabs/TabOptions.cs
+++ src/Masa.Blazor/Presets/PageTabs/TabOptions.cs
@@ -12,26 +12,29 @@ public class TabOptions
 
     public string? TitleStyle { get; set; }
 
-    public TabOptions()
+    public bool Closeable { get; set; } = true;
+
+    public TabOptions(bool closeable = true)
     {
+        Closeable = closeable;
     }
 
-    public TabOptions(string? title)
+    public TabOptions(string? title, bool closeable = true) : this(closeable)
     {
         Title = title;
     }
 
-    public TabOptions(string? title, string? icon) : this(title)
+    public TabOptions(string? title, string? icon, bool closeable = true) : this(title, closeable)
     {
         Icon = icon;
     }
 
-    public TabOptions(string? title, string? icon, string? titleClass) : this(title, icon)
+    public TabOptions(string? title, string? icon, string? titleClass, bool closeable = true) : this(title, icon, closeable)
     {
         TitleClass = titleClass;
     }
 
-    public TabOptions(string? title, string? icon, string? titleClass, string? @class) : this(title, icon, titleClass)
+    public TabOptions(string? title, string? icon, string? titleClass, string? @class, bool closeable = true) : this(title, icon, titleClass, closeable)
     {
         Class = @class;
     }
