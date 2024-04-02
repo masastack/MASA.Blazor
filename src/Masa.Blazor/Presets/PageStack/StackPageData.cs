a532bd4bf6cce3e0b39a3ff796ff776743428692
capdiem <capdiem@live.com>
2024-04-02 11:54:51 +0800

    🐛 fix(PageStack): incorrect behavior when clicking the browser back button (#1814)

---
+++ src/Masa.Blazor/Presets/PageStack/StackPageData.cs
@@ -0,0 +1,33 @@
+﻿namespace Masa.Blazor.Presets;
+
+public class StackPageData
+{
+    public StackPageData(string absolutePath)
+    {
+        Active = true;
+        AbsolutePath = absolutePath;
+        Pattern = absolutePath.ToLower();
+    }
+
+    public StackPageData(string pattern, string absolutePath)
+    {
+        Active = true;
+        AbsolutePath = absolutePath;
+        IsSelf = true;
+        Pattern = pattern.ToLower();
+    }
+
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public string AbsolutePath { get; private set; }
+
+    public string Pattern { get; init; }
+
+    public bool IsSelf { get; init; }
+
+    public bool Active { get; set; }
+
+    public string Selector { get; set; }
+    
+    public void UpdatePath(string absolutePath) => AbsolutePath = absolutePath;
+}
