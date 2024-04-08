2706863033bdb9b677640ffce8b502b30db068be
capdiem <capdiem@live.com>
2024-04-08 16:54:35 +0800

    🚚 refactor(Grid): merge code from BlazorComponent (#1827)

--- src/Masa.Blazor/Components/Grid/MCol.cs
+++ src/Masa.Blazor/Components/Grid/MCol.cs
@@ -1,103 +1,157 @@
 ﻿using Masa.Blazor.Extensions;
-using System.Text.RegularExpressions;
 
-namespace Masa.Blazor
+namespace Masa.Blazor;
+
+public class MCol : Container
 {
-    public partial class MCol : BCol
+    [Parameter] [MasaApiParameter("div")] public string Tag { get; set; } = "div";
+
+    [Parameter] public StringNumber? Sm { get; set; }
+
+    [Parameter] public StringNumber? Md { get; set; }
+
+    [Parameter] public StringNumber? Lg { get; set; }
+
+    [Parameter] public StringNumber? Xl { get; set; }
+
+    /// <summary>
+    /// 'auto', 'start', 'end', 'center', 'baseline', 'stretch'
+    /// </summary>
+    [Parameter]
+    public StringEnum<AlignTypes>? Align { get; set; }
+
+    [Parameter] public StringNumber? OrderLg { get; set; }
+
+    [Parameter] public StringNumber? OrderMd { get; set; }
+
+    [Parameter] public StringNumber? OrderSm { get; set; }
+
+    [Parameter] public StringNumber? OrderXl { get; set; }
+
+    [Parameter] public StringNumber? OffsetLg { get; set; }
+
+    [Parameter] public StringNumber? OffsetMd { get; set; }
+
+    [Parameter] public StringNumber? OffsetSm { get; set; }
+
+    [Parameter] public StringNumber? OffsetXl { get; set; }
+
+    [Parameter] public StringNumber? Flex { get; set; }
+
+    [Parameter] public StringNumber? Cols { get; set; }
+
+    [Parameter] public StringNumber? Order { get; set; }
+
+    [Parameter] public StringNumber? Offset { get; set; }
+
+    private static readonly Regex FlexStyleRegex = new(@"^(\d+(?:\.\d+)?)(px|em|rem|%)?$", RegexOptions.Compiled);
+
+    protected override string TagName => Tag;
+
+    protected override IEnumerable<string> BuildComponentClass()
     {
-        [Parameter]
-        public StringNumber? Sm { get; set; }
-
-        [Parameter]
-        public StringNumber? Md { get; set; }
-
-        [Parameter]
-        public StringNumber? Lg { get; set; }
-
-        [Parameter]
-        public StringNumber? Xl { get; set; }
-
-        /// <summary>
-        /// 'auto', 'start', 'end', 'center', 'baseline', 'stretch'
-        /// </summary>
-        [Parameter]
-        public StringEnum<AlignTypes>? Align { get; set; }
-
-        [Parameter]
-        public StringNumber? OrderLg { get; set; }
-
-        [Parameter]
-        public StringNumber? OrderMd { get; set; }
-
-        [Parameter]
-        public StringNumber? OrderSm { get; set; }
-
-        [Parameter]
-        public StringNumber? OrderXl { get; set; }
-
-        [Parameter]
-        public StringNumber? OffsetLg { get; set; }
-
-        [Parameter]
-        public StringNumber? OffsetMd { get; set; }
-
-        [Parameter]
-        public StringNumber? OffsetSm { get; set; }
-
-        [Parameter]
-        public StringNumber? OffsetXl { get; set; }
-
-        [Parameter]
-        public StringNumber? Flex { get; set; }
-
-        protected override void SetComponentClass()
+        if (Cols == null || (Sm == null && Md == null && Lg == null && Xl == null))
         {
-            CssProvider
-                .Apply(cssBuilder =>
-                {
-                    cssBuilder
-                        .AddIf(() => $"col",
-                            () => Cols == null || (Sm == null && Md == null && Lg == null && Xl == null))
-                        .AddIf(() => $"col-{Cols!.Value}", () => Cols != null)
-                        .AddIf(() => $"col-sm-{Sm!.Value}", () => Sm != null)
-                        .AddIf(() => $"col-md-{Md!.Value}", () => Md != null)
-                        .AddIf(() => $"col-lg-{Lg!.Value}", () => Lg != null)
-                        .AddIf(() => $"col-xl-{Xl!.Value}", () => Xl != null)
-                        .AddIf(() => $"offset-{Offset!.Value}", () => Offset != null)
-                        .AddIf(() => $"offset-lg-{OffsetLg}", () => OffsetLg != null)
-                        .AddIf(() => $"offset-md-{OffsetMd}", () => OffsetMd != null)
-                        .AddIf(() => $"offset-sm-{OffsetSm}", () => OffsetSm != null)
-                        .AddIf(() => $"offset-xl-{OffsetXl}", () => OffsetXl != null)
-                        .AddIf(() => $"order-{Order!.Value}", () => Order != null)
-                        .AddIf(() => $"order-lg-{OrderLg}", () => OrderLg != null)
-                        .AddIf(() => $"order-md-{OrderMd}", () => OrderMd != null)
-                        .AddIf(() => $"order-sm-{OrderSm}", () => OrderSm != null)
-                        .AddIf(() => $"order-xl-{OrderXl}", () => OrderXl != null)
-                        .AddIf(Align!.ToString(() =>
-                                $"align-self-{Align}",
-                            ("align-self-auto", AlignTypes.Auto),
-                            ("align-self-start", AlignTypes.Start),
-                            ("align-self-center", AlignTypes.Center),
-                            ("align-self-end", AlignTypes.End),
-                            ("align-self-baseline", AlignTypes.Baseline),
-                            ("align-self-stretch", AlignTypes.Stretch)), () => Align != null)
-                        .AddIf(SetHostFlexStyle, () => Flex != null);
-                });
+            yield return "col";
         }
 
-        private string SetHostFlexStyle()
+        if (Cols != null)
         {
-            return Flex!.Match(str =>
-                {
-                    if (Regex.Match(str, "^\\d+(\\.\\d+)?(px|em|rem|%)$").Success)
-                    {
-                        return $"flex: 0 0 {str}";
-                    }
-
-                    return $"flex: {str}";
-                },
-                num => $"flex: {num} {num} auto",
-                _ => string.Empty);
+            yield return $"col-{Cols.Value}";
         }
+
+        if (Sm != null)
+        {
+            yield return $"col-sm-{Sm.Value}";
+        }
+
+        if (Md != null)
+        {
+            yield return $"col-md-{Md.Value}";
+        }
+
+        if (Lg != null)
+        {
+            yield return $"col-lg-{Lg.Value}";
+        }
+
+        if (Xl != null)
+        {
+            yield return $"col-xl-{Xl.Value}";
+        }
+
+        if (Offset != null)
+        {
+            yield return $"offset-{Offset.Value}";
+        }
+
+        if (OffsetLg != null)
+        {
+            yield return $"offset-lg-{OffsetLg}";
+        }
+
+        if (OffsetMd != null)
+        {
+            yield return $"offset-md-{OffsetMd}";
+        }
+
+        if (OffsetSm != null)
+        {
+            yield return $"offset-sm-{OffsetSm}";
+        }
+
+        if (OffsetXl != null)
+        {
+            yield return $"offset-xl-{OffsetXl}";
+        }
+
+        if (Order != null)
+        {
+            yield return $"order-{Order.Value}";
+        }
+
+        if (OrderLg != null)
+        {
+            yield return $"order-lg-{OrderLg}";
+        }
+
+        if (OrderMd != null)
+        {
+            yield return $"order-md-{OrderMd}";
+        }
+
+        if (OrderSm != null)
+        {
+            yield return $"order-sm-{OrderSm}";
+        }
+
+        if (OrderXl != null)
+        {
+            yield return $"order-xl-{OrderXl}";
+        }
+
+        if (Align != null)
+        {
+            yield return Align.ToString(() => $"align-self-{Align}",
+                ("align-self-auto", AlignTypes.Auto),
+                ("align-self-start", AlignTypes.Start),
+                ("align-self-center", AlignTypes.Center),
+                ("align-self-end", AlignTypes.End),
+                ("align-self-baseline", AlignTypes.Baseline),
+                ("align-self-stretch", AlignTypes.Stretch));
+        }
+
+        if (Flex != null)
+        {
+            yield return SetHostFlexStyle();
+        }
+    }
+
+    private string SetHostFlexStyle()
+    {
+        return Flex!.Match(
+            str => FlexStyleRegex.IsMatch(str) ? $"flex: 0 0 {str}" : $"flex: {str}",
+            num => $"flex: {num} {num} auto",
+            _ => string.Empty);
     }
 }
