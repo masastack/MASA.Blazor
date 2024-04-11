00f81eb0656c0ba3ce73eda547379f4a3c5c1090
capdiem <capdiem@live.com>
2024-04-11 13:57:04 +0800

    🆕 feat(Snackbar): Use reverse-surface color as the bgcolor

--- src/Masa.Blazor/Components/Snackbar/MSnackbar.razor.cs
+++ src/Masa.Blazor/Components/Snackbar/MSnackbar.razor.cs
@@ -1,81 +1,96 @@
-﻿using Microsoft.AspNetCore.Components.Web;
-using System.Timers;
+﻿using System.Timers;
+using StyleBuilder = Masa.Blazor.Core.StyleBuilder;
 using Timer = System.Timers.Timer;
 
-namespace Masa.Blazor
+namespace Masa.Blazor;
+
+public partial class MSnackbar : MasaComponentBase
 {
-    public partial class MSnackbar : BSnackbar, IThemeable, ISnackbar
+    [Inject] private MasaBlazor MasaBlazor { get; set; } = null!;
+        
+    [Parameter] public bool Absolute { get; set; }
+
+    [Parameter] public bool Value { get; set; }
+
+    [Parameter] public EventCallback<bool> ValueChanged { get; set; }
+
+    [Parameter] public EventCallback OnClosed { get; set; }
+
+    [Parameter] public bool Bottom { get; set; }
+
+    [Parameter] public bool Centered { get; set; }
+
+    [Parameter] public bool Left { get; set; }
+
+    [Parameter] public bool MultiLine { get; set; }
+
+    [Parameter] public bool Right { get; set; }
+
+    [Parameter] public bool Top { get; set; }
+
+    [Parameter] public bool Vertical { get; set; }
+
+    [Parameter] public bool Shaped { get; set; }
+
+    [Parameter] [MasaApiParameter(5000)] public int Timeout { get; set; } = 5000;
+
+    [Parameter] public string? Color { get; set; }
+
+    [Parameter] public StringNumber? Elevation { get; set; }
+
+    [Parameter] public bool Tile { get; set; }
+
+    [Parameter] public StringBoolean? Rounded { get; set; }
+
+    [Parameter] public string? Action { get; set; }
+
+    [Parameter] public RenderFragment? ActionContent { get; set; }
+
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    [Parameter] public string? Transition { get; set; }
+
+    [Parameter] public string? ContentClass { get; set; }
+
+    [Parameter] public bool Text { get; set; }
+
+    [Parameter] public bool Outlined { get; set; }
+
+    [Parameter] public bool Dark { get; set; }
+
+    [Parameter] public bool Light { get; set; }
+
+    [CascadingParameter(Name = "IsDark")] public bool CascadingIsDark { get; set; }
+
+    public bool IsDark
     {
-        [Parameter]
-        public bool Absolute { get; set; }
-
-        [Parameter]
-        public bool Value { get; set; }
-
-        [Parameter]
-        public EventCallback<bool> ValueChanged { get; set; }
-
-        [Parameter]
-        public EventCallback OnClosed { get; set; }
-
-        [Parameter]
-        public bool Bottom { get; set; }
-
-        [Parameter]
-        public bool Centered { get; set; }
-
-        [Parameter]
-        public bool Left { get; set; }
-
-        [Parameter]
-        public bool MultiLine { get; set; }
-
-        [Parameter]
-        public bool Right { get; set; }
-
-        [Parameter]
-        public bool Top { get; set; }
-
-        [Parameter]
-        public bool Vertical { get; set; }
-
-        [Parameter]
-        public bool Shaped { get; set; }
-
-        [Parameter]
-        [MasaApiParameter(5000)]
-        public int Timeout { get; set; } = 5000;
-
-        [Parameter]
-        public string? Color { get; set; }
-
-        [Parameter]
-        public StringNumber? Elevation { get; set; }
-
-        [Parameter]
-        public bool Tile { get; set; }
-
-        [Parameter]
-        public StringBoolean? Rounded { get; set; }
-
-        [Parameter]
-        public string? Action { get; set; }
-
-        [Parameter]
-        public RenderFragment? ActionContent { get; set; }
-
-        private const string ROOT_CSS = "m-snack";
-        internal const string ROOT_CSS_SELECTOR = $".{ROOT_CSS}";
-
-        private Timer? Timer { get; set; }
-
-        [Inject] private MasaBlazor MasaBlazor { get; set; } = null!;
-
-        private bool IndependentTheme => (IsDirtyParameter(nameof(Dark)) && Dark) || (IsDirtyParameter(nameof(Light)) && Light);
-
-        protected override void OnParametersSet()
+        get
         {
-            base.OnParametersSet();
+            if (Dark)
+            {
+                return true;
+            }
+
+            if (Light)
+            {
+                return false;
+            }
+
+            return CascadingIsDark;
+        }
+    }
+
+    private const string ROOT_CSS = "m-snack";
+    internal const string ROOT_CSS_SELECTOR = $".{ROOT_CSS}";
+
+    private Timer? Timer { get; set; }
+
+    private bool IndependentTheme =>
+        (IsDirtyParameter(nameof(Dark)) && Dark) || (IsDirtyParameter(nameof(Light)) && Light);
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
 
 #if NET8_0_OR_GREATER
             if (MasaBlazor.IsSsr && !IndependentTheme)
@@ -84,112 +99,111 @@ namespace Masa.Blazor
             }
 #endif
 
-            Transition ??= "m-snack-transition";
+        Transition ??= "m-snack-transition";
 
-            if (Value && Timeout > 0)
-            {
-                if (Timer == null)
-                {
-                    Timer = new Timer(Timeout);
-                    Timer.Elapsed += Timer_Elapsed;
-                }
-
-                Timer.Enabled = true;
-            }
-        }
-
-        protected override void SetComponentClass()
+        if (Value && Timeout > 0)
         {
-            var rootCss = ROOT_CSS;
-            CssProvider
-                .Apply(cssBuilder =>
-                {
-                    cssBuilder
-                        .Add(rootCss)
-                        .AddIf($"{rootCss}--absolute", () => Absolute)
-                        .AddIf($"{rootCss}--active", () => Value)
-                        .AddIf($"{rootCss}--bottom", () => Bottom || !Top)
-                        .AddIf($"{rootCss}--centered", () => Centered)
-                        .AddIf($"{rootCss}--has-background", () => !Text && !Outlined)
-                        .AddIf($"{rootCss}--left", () => Left)
-                        .AddIf($"{rootCss}--multi-line", () => MultiLine && !Vertical)
-                        .AddIf($"{rootCss}--right", () => Right)
-                        .AddIf($"{rootCss}--text", () => Text)
-                        .AddIf($"{rootCss}--top", () => Top)
-                        .AddIf($"{rootCss}--vertical", () => Vertical);
-                }, styleBuilder =>
-                {
-                    styleBuilder
-                        .Add("padding-bottom: 0px")
-                        .Add("padding-top: 64px");
-                })
-                .Apply("wrap", cssBuilder =>
-                {
-                    cssBuilder
-                        .Add($"{rootCss}__wrapper")
-                        .Add("m-sheet")
-                        .AddIf("m-sheet--outlined", () => Outlined)
-                        .AddIf("m-sheet--shaped", () => Shaped)
-                        .AddBackgroundColor(Color, () => !Text && !Outlined)
-                        .AddTextColor(Color, () => Text || Outlined)
-                        .AddRounded(Rounded, Tile)
-                        .AddElevation(Elevation)
-                        .AddTheme(IsDark, IndependentTheme);
-                }, styleBuilder =>
-                {
-                    styleBuilder.AddBackgroundColor(Color);
-                    styleBuilder.AddTextColor(Color, () => Text || Outlined);
-                })
-                .Apply("content", cssBuilder =>
-                {
-                    cssBuilder
-                        .Add($"{rootCss}__content")
-                        .Add(ContentClass);
-                })
-                .Apply("action", cssBuilder =>
-                {
-                    cssBuilder
-                        .Add($"{rootCss}__action");
-                });
+            if (Timer == null)
+            {
+                Timer = new Timer(Timeout);
+                Timer.Elapsed += Timer_Elapsed;
+            }
 
-            AbstractProvider
-                .ApplySnackbarDefault()
-                .Apply<BButton, MButton>(attrs =>
-                {
-                    attrs[nameof(Class)] = "m-snack__btn";
-                    attrs[nameof(MButton.Text)] = true;
-                    attrs[nameof(MButton.OnClick)] = EventCallback.Factory.Create<MouseEventArgs>(this, async () =>
-                    {
-                        Value = false;
-                        Timer?.Stop();
-                        if (ValueChanged.HasDelegate)
-                        {
-                            await ValueChanged.InvokeAsync(Value);
-                        }
-
-                        if (OnClosed.HasDelegate)
-                        {
-                            await OnClosed.InvokeAsync();
-                        }
-                    });
-                });
+            Timer.Enabled = true;
         }
+    }
 
-        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+    private Block _block = new(ROOT_CSS);
+
+    protected override IEnumerable<string> BuildComponentClass()
+    {
+        return _block.Modifier(Absolute)
+            .And("active", Value)
+            .And("bottom", Bottom || !Top)
+            .And(Centered)
+            .And("has-background", !Text && !Outlined)
+            .And(Left)
+            .And("multi-line", MultiLine && !Vertical)
+            .And(Right)
+            .And(Text)
+            .And(Top)
+            .And(Vertical)
+            .GenerateCssClasses();
+    }
+
+    protected override IEnumerable<string> BuildComponentStyle()
+    {
+        return StyleBuilder.Create()
+            .Add("padding-bottom", "0")
+            .Add("padding-top", "64px")
+            .GenerateCssStyles();
+    }
+
+    private string GetWrapperClass()
+    {
+        return _block.Element("wrapper")
+            .AddClass("m-sheet")
+            .AddClass("m-sheet--outlined", Outlined)
+            .AddClass("m-sheet--shaped", Shaped)
+            .AddBackgroundColor(Color, !Text && !Outlined)
+            .AddTextColor(Color, Text || Outlined)
+            .AddRounded(Rounded, Tile)
+            .AddElevation(Elevation)
+            .AddTheme(IsDark, IndependentTheme)
+            .Build();
+    }
+
+    private string GetWrapperStyle()
+    {
+        return StyleBuilder.Create()
+            .AddBackgroundColor(Color)
+            .AddTextColor(Color, () => Text || Outlined)
+            .Build();
+    }
+
+    private void HandleOnAction()
+    {
+        Timer?.Stop();
+
+        if (ValueChanged.HasDelegate)
+        {
+            _ = ValueChanged.InvokeAsync(false);
+        }
+        else
         {
             Value = false;
-            if (ValueChanged.HasDelegate)
-            {
-                InvokeAsync(() => ValueChanged.InvokeAsync(Value));
-            }
-
-            if (OnClosed.HasDelegate)
-            {
-                InvokeAsync(() => OnClosed.InvokeAsync());
-            }
-
-            Timer!.Enabled = false;
-            InvokeStateHasChanged();
         }
+
+        _ = OnClosed.InvokeAsync();
+    }
+
+    private void HandleOnPointerEnter()
+    {
+        Timer?.Stop();
+    }
+
+    private void HandleOnPointerLeave()
+    {
+        if (Value && Timeout > 0)
+        {
+            Timer?.Start();
+        }
+    }
+
+    private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+    {
+        Value = false;
+        if (ValueChanged.HasDelegate)
+        {
+            InvokeAsync(() => ValueChanged.InvokeAsync(Value));
+        }
+
+        if (OnClosed.HasDelegate)
+        {
+            InvokeAsync(() => OnClosed.InvokeAsync());
+        }
+
+        Timer!.Enabled = false;
+        InvokeStateHasChanged();
     }
 }
