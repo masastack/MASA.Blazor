cef9571986b18d9c0949fd910e00ceb839ce243b
capdiem <capdiem@live.com>
2024-04-29 13:14:16 +0800

    update demo

---
+++ docs/Masa.Blazor.Docs/Pages/PageStackPage3.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Masa.Blazor.Docs.Pages;
+
+public partial class PageStackPage3
+{
+    private string sourceCode =
+        """
+        <PStackPageBarInit Title="Page 3">
+            <ActionContent>
+                <MButton IconName="mdi-home"
+                         OnClick="@(() => NavController.Tab("/blazor/examples/page-stack/tab3"))">
+                </MButton>
+            </ActionContent>
+        </PStackPageBarInit>
+        """;
+}
---
+++ docs/Masa.Blazor.Docs/Pages/PageStackPage4.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Masa.Blazor.Docs.Pages;
+
+public partial class PageStackPage4
+{
+    private string sourceCode =
+        """
+        @inject PageStackNavController NavController
+        
+        <ul>
+            <li><a @onclick="@(() => NavController.Pop("AAA"))">AAA</a></li>
+            <li><a @onclick="@(() => NavController.Pop("BBB"))">BBB</a></li>
+            <li><a @onclick="@(() => NavController.Pop("CCC"))">CCC</a></li>
+        </ul>
+        """;
+}
