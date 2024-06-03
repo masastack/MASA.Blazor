fb6768aa217706c23057925c681e9f867549efe0
capdiem <capdiem@live.com>
2024-06-03 15:46:03 +0800

    🐛 fix(PullRefresh): should not preventDefault on touch move (#1950)

--- src/Masa.Blazor/Components/PullRefresh/MPullRefresh.razor.cs
+++ src/Masa.Blazor/Components/PullRefresh/MPullRefresh.razor.cs
@@ -88,7 +88,7 @@ public partial class MPullRefresh
                 Passive = false
             }, new EventListenerExtras()
             {
-                PreventDefault = true,
+                PreventDefault = false,
                 Throttle = 16
             });
         }
@@ -141,6 +141,11 @@ public partial class MPullRefresh
 
             if (_reachTop && _toucher.DeltaY >= 0 && _toucher.IsVertical)
             {
+                // HACK: In JavaScript, should dynamically set the `preventDefault()` here,
+                // but in Blazor, it's difficult to do that. so we set it to false in `AddHtmlElementEventListener`.
+                // If it's necessary, should refactor this part.
+                // args.PreventDefault();
+
                 await _throttleTask.RunAsync(async () =>
                 {
                     if (IsTouchable)
