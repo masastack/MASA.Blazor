76a32dcea31c978c58ed12783e02b68399ffbb6b
capdiem <capdiem@live.com>
2024-07-24 23:12:05 +0800

    🆕 : add the new component MToggle

---
+++ src/Masa.Blazor/Components/Toggle/MToggle.cs
@@ -0,0 +1,30 @@
+﻿namespace Masa.Blazor;
+
+public class MToggle<TData> : ComponentBase
+{
+    [Parameter] public bool Value { get; set; }
+
+    [Parameter] public EventCallback<bool> ValueChanged { get; set; }
+
+    [Parameter] public TData? DataOn { get; set; }
+
+    [Parameter] public TData? DataOff { get; set; }
+
+    [Parameter] public RenderFragment<ToggleContext<TData>>? ChildContent { get; set; }
+
+    private async Task OnToggle()
+    {
+        Value = !Value;
+        await ValueChanged.InvokeAsync(Value);
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.AddContent(
+            0,
+            ChildContent?.Invoke(new ToggleContext<TData>(OnToggle, Value ? DataOn : DataOff, Value))
+        );
+    }
+}
+
+public record ToggleContext<TData>(Func<Task> Toggle, TData? Data, bool On);
