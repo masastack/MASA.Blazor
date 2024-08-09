3de1102d789d1d274b4d8c71b7b1069e3449c6c2
capdiem <capdiem@live.com>
2024-08-09 15:34:13 +0800

    🎬 docs: update pagestack demo

--- docs/Masa.Blazor.Docs/Pages/PageStackPage3.razor.cs
+++ docs/Masa.Blazor.Docs/Pages/PageStackPage3.razor.cs
@@ -11,5 +11,7 @@ public partial class PageStackPage3
                 </MButton>
             </ActionContent>
         </PStackPageBarInit>
+
+        <a @onclick="@(() => NavController.GoBackToPage("/blazor/examples/page-stack/page1"))">Go back to page 1</a>
         """;
 }
