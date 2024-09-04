0d4de394b65d4ef7ee315d6a806c7d6ceb83fd26
capdiem <capdiem@live.com>
2024-09-04 09:25:14 +0800

    🐛 (Service): breakpoint service doesn't work when interactive per page/component enabled (#2131)

---
+++ src/Masa.Blazor/Utils/Throttle.cs
@@ -0,0 +1,24 @@
+﻿namespace Masa.Blazor.Utils;
+
+public class Throttle(TimeSpan delay)
+{
+    private DateTime _lastExecutionTime = DateTime.MinValue;
+    private readonly object _lock = new();
+
+    public Throttle(int delay) : this(TimeSpan.FromMilliseconds(delay))
+    {
+    }
+
+    public void Execute(Action action)
+    {
+        lock (_lock)
+        {
+            var now = DateTime.UtcNow;
+            if (now - _lastExecutionTime >= delay)
+            {
+                _lastExecutionTime = now;
+                action();
+            }
+        }
+    }
+}
