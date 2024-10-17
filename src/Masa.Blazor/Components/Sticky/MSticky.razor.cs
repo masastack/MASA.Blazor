3dbcdf6284650e468a93fd7466ad2d02f0834889
capdiem <capdiem@live.com>
2024-10-17 17:44:55 +0800

    wip

---
+++ src/Masa.Blazor/Components/Sticky/MSticky.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace Masa.Blazor;
+
+public partial class MSticky : MasaComponentBase
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    [Parameter] public StringNumber? OffsetTop { get; set; }
+
+    [Parameter] public bool Disabled { get; set; }
+
+    [Parameter] public string? Container { get; set; }
+
+    [Parameter] public int ZIndex { get; set; }
+
+    private static Block _block = new("m-sticky");
+
+    private string? GetContentStyle()
+    {
+        return null;
+    }
+}
