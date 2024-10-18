72e10801e7582fd970dfa3878c094e9e95450909
capdiem <capdiem@live.com>
2024-10-18 18:04:47 +0800

    wip

---
+++ src/Masa.Blazor/Components/Sticky/MSticky.cs
@@ -0,0 +1,22 @@
+﻿namespace Masa.Blazor.Components.Sticky;
+
+public class MSticky : MasaComponentBase
+{
+    [Parameter] public RenderFragment<bool>? ChildContent { get; set; }
+
+    [Parameter] public double Top { get; set; }
+
+    [Parameter] public double Bottom { get; set; }
+
+    private static Block _block = new("m-sticky");
+    private ModifierBuilder _modifierBuilder = _block.CreateModifierBuilder();
+
+    private bool _isSticky;
+
+    protected override IEnumerable<string?> BuildComponentClass()
+    {
+        yield return _modifierBuilder.Add("sticky", _isSticky).Build();
+    }
+    
+    
+}
