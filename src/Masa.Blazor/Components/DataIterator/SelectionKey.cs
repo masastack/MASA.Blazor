b5d6e55081a7b35c4c2051ac574d21f3db33fa25
capdiem <capdiem@live.com>
2024-12-04 17:22:07 +0800

    🐛 fix(DataTable): selected items should not be reset in server-side pagination

---
+++ src/Masa.Blazor/Components/DataIterator/SelectionKey.cs
@@ -0,0 +1,16 @@
+﻿namespace Masa.Blazor.Components.DataTable;
+
+public record SelectionKey<TItem>(string Key, TItem? Item)
+{
+    public virtual bool Equals(SelectionKey<TItem>? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Key == other.Key;
+    }
+
+    public override int GetHashCode()
+    {
+        return Key.GetHashCode();
+    }
+}
