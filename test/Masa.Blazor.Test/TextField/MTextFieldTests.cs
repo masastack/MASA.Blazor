9a4048f34b5ef2b42ddd543671ce7b7fb0e4de83
capdiem <capdiem@live.com>
2024-12-23 16:38:13 +0800

    🐛 fix(TextField): support nullable numeric types (#2294)
    * 🐛 fix(TextField): support nullable numeric types
    
    * add tests

--- test/Masa.Blazor.Test/TextField/MTextFieldTests.cs
+++ test/Masa.Blazor.Test/TextField/MTextFieldTests.cs
@@ -20,7 +20,6 @@ namespace Masa.Blazor.Test.TextField
             Assert.IsTrue(inputDiv.ClassList.Contains("m-text-field"));
             Assert.IsTrue(inputDiv.ClassList.Contains("m-text-field--is-booted"));
             Assert.IsTrue(inputDiv.ClassList.Contains("m-input"));
-
         }
 
         [DataTestMethod]
@@ -445,6 +444,13 @@ namespace Masa.Blazor.Test.TextField
 
             // Assert
             Assert.IsTrue(hasValidateOnBlurClass);
+        }
+
+        [TestMethod]
+        public void IsNullable()
+        {
+            Assert.IsTrue(MTextField<double?>.IsNullable<double?>());
+            Assert.IsFalse(MTextField<double>.IsNullable<double>());
         }
     }
 }
