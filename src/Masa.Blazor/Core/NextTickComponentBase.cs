f92b47ec06c29679843884c38d81c5a301b8d9e3
capdiem <capdiem@live.com>
2025-02-17 09:28:04 +0800

    🆕 feat(Tour): new component based on Driver.js (#2322)

--- src/Masa.Blazor/Core/NextTickComponentBase.cs
+++ src/Masa.Blazor/Core/NextTickComponentBase.cs
@@ -1,11 +1,9 @@
 ﻿namespace Masa.Blazor;
 
-public abstract class NextTickComponentBase : ComponentBase, IAsyncDisposable
+public abstract class NextTickComponentBase : DisposableComponentBase
 {
     private readonly Queue<Func<Task>> _nextTickQueue = new();
 
-    protected bool IsDisposed { get; private set; }
-    
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (_nextTickQueue.Count > 0)
@@ -114,38 +112,5 @@ public abstract class NextTickComponentBase : ComponentBase, IAsyncDisposable
                 break;
             }
         }
-    }
-
-    protected virtual ValueTask DisposeAsyncCore() => ValueTask.CompletedTask;
-
-    public async ValueTask DisposeAsync()
-    {
-        if (IsDisposed)
-        {
-            return;
-        }
-
-        try
-        {
-            await DisposeAsyncCore().ConfigureAwait(false);
-        }
-        catch (JSDisconnectedException)
-        {
-            // ignored
-        }
-        // HACK: remove this after https://github.com/dotnet/aspnetcore/issues/52119 is fixed
-        catch (JSException e) when (e.Message.Contains("has it been disposed")
-                                    && (OperatingSystem.IsWindows() || OperatingSystem.IsAndroid() ||
-                                        OperatingSystem.IsIOS()))
-        {
-            // ignored
-        }
-        catch (InvalidOperationException e) when (e.Message.Contains("prerendering"))
-        {
-            // ignored
-        }
-
-        GC.SuppressFinalize(this);
-        IsDisposed = true;
     }
 }
