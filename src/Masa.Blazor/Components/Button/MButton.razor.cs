masastack/MASA.Blazor

commit 2087a2fa63f96dc3d9aa511ac8c816cb6aba5ef0

🐛 fix(activator): handle click event propagation correctly (#2332)

src/Masa.Blazor/Components/Button/MButton.razor.cs
@@ -219,15 +219,10 @@ namespace Masa.Blazor
 #endif
 
             Attributes["ripple"] = Ripple;
-        }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            await base.OnAfterRenderAsync(firstRender);
-
-            if (firstRender)
+            if (OnClick.HasDelegate)
             {
-                await Js.AddClickEventListener(Ref, HandleOnClick, OnClickStopPropagation, OnClickPreventDefault);
+                Console.Out.WriteLine("[MButton] OnClick has delegate");
+                Attributes["onclick"] = EventCallback.Factory.Create<MouseEventArgs>(this, HandleOnClick);
             }
         }
 
@@ -238,16 +233,9 @@ namespace Masa.Blazor
                 await Js.InvokeVoidAsync(JsInteropConstants.Blur, Ref);
             }
 
-            await InvokeCallbackAsync(OnClick, args);
+            await OnClick.InvokeAsync(args);
 
             await ToggleAsync();
-        }
-
-        protected override async ValueTask DisposeAsyncCore()
-        {
-            await Js.RemoveClickEventListener(Ref, "click");
-
-            await base.DisposeAsyncCore();
         }
     }
 }

src/Masa.Blazor.JS/src/globals.d.ts
@@ -198,6 +198,14 @@ declare global {
     class?: string;
   }
 
+  interface EventTarget {
+    _blazorEvents_1?: {
+      handlers: {
+        [key: string]: any
+      }
+    }
+  }
+
   namespace DotNet {
     interface DotNetObject {
       dispose(): void;

src/Masa.Blazor.JS/src/mixins/activatable.ts
@@ -88,7 +88,12 @@ class Activatable extends Delayable {
       listeners.click = (e: MouseEvent) => {
         if (this.activator) this.activator.focus();
 
-        e.stopPropagation();
+        // if there is no click event from blazor on the element, stop propagation
+        const target = e.composedPath().find(e => e === this.activator)
+        console.log('target', target, target._blazorEvents_1)
+        if (!target?._blazorEvents_1?.handlers?.click) {
+          e.stopPropagation();
+        }
 
         this.dotNetHelper.invokeMethodAsync("OnClick", parseMouseEvent(e));
 
@@ -98,7 +103,11 @@ class Activatable extends Delayable {
 
     if (this.openOnFocus) {
       listeners.focus = (e: FocusEvent) => {
-        e.stopPropagation();
+        // if there is no focus event from blazor on the element, stop propagation
+        const target = e.composedPath().find(e => e === this.activator)
+        if (!target?._blazorEvents_1?.handlers?.focus) {
+          e.stopPropagation();
+        }
 
         this.runDelay("open");
       };

src/Masa.Blazor/Components/Button/MButton.razor
@@ -13,6 +13,8 @@
             Value="@Key"
             id="@Id"
             ReferenceCaptureAction="reference => Ref = reference"
+            __internal_stopPropagation_onclick="@OnClickStopPropagation"
+            __internal_preventDefault_onclick="@OnClickPreventDefault"
             @attributes="Attributes">
             @GenContent()
         </KeyTransitionElement>
@@ -27,6 +29,8 @@
             Value="@(Show ?? true)"
             id="@Id"
             ReferenceCaptureAction="reference => Ref = reference"
+            __internal_stopPropagation_onclick="@OnClickStopPropagation"
+            __internal_preventDefault_onclick="@OnClickPreventDefault"
             @attributes="Attributes">
             @GenContent()
         </ShowTransitionElement>

src/Masa.Blazor/Components/Card/MCard.razor.cs
@@ -92,7 +92,11 @@ public partial class MCard : MSheet, IRoutable
         (Tag, Attributes) = _router.GenerateRouteLink();
 
         Attributes["ripple"] = Ripple && IsClickable;
-        Attributes["onclick"] = EventCallback.Factory.Create<MouseEventArgs>(this, args => InvokeCallbackAsync(OnClick, args));
+
+        if (OnClick.HasDelegate)
+        {
+            Attributes["onclick"] = EventCallback.Factory.Create<MouseEventArgs>(this, e => OnClick.InvokeAsync(e));
+        }
     }
 
     private static Block _block = new("m-card");

src/Masa.Blazor/Core/MasaComponentBase.cs
@@ -423,31 +423,4 @@ public abstract class MasaComponentBase : NextTickComponentBase, IHandleEvent
     }
 
     #endregion
-
-    /// <summary>
-    /// Invoke a callback event and use the error handler to catch exceptions
-    /// when an error occurs.
-    /// </summary>
-    /// <param name="callback"></param>
-    /// <param name="args"></param>
-    /// <typeparam name="TEventArgs"></typeparam>
-    protected async Task InvokeCallbackAsync<TEventArgs>(EventCallback<TEventArgs> callback, TEventArgs args)
-    {
-        try
-        {
-            if (callback.HasDelegate)
-            {
-                await callback.InvokeAsync(args);
-            }
-        }
-        catch (Exception e)
-        {
-            if (ErrorHandler is null)
-            {
-                throw;
-            }
-
-            await ErrorHandler.HandleExceptionAsync(e);
-        }
-    }
 }

src/Masa.Blazor/wwwroot/js/mixins/activatable/index.js
@@ -1,2 +1,2 @@
-import{d as t}from"../../chunks/helper.js";import{p as e}from"../../chunks/EventType.js";class s{constructor(t,e,s){this.openDelay=t,this.closeDelay=e,this.dotNetHelper=s}clearDelay(){clearTimeout(this.openTimeout),clearTimeout(this.closeTimeout)}runDelay(t,e){this.clearDelay();const s=parseInt(this[`${t}Delay`],10);this[`${t}Timeout`]=setTimeout(e||(()=>{const e={open:!0,close:!1}[t];this.setActive(e)}),s)}setActive(t){this.isActive!=t&&(this.isActive=t,this.dotNetHelper.invokeMethodAsync("SetActive",this.isActive))}resetDelay(t,e){this.openDelay=t,this.closeDelay=e}}class i extends s{constructor(e,s,i,o,n,r,c,p,h){super(c,p,h),this.activatorListeners={},this.popupListeners={},this.isActive=e,this.activator=t(s),this.disabled=i,this.openOnClick=o,this.openOnHover=n,this.openOnFocus=r,this.dotNetHelper=h}resetActivator(t){const e=document.querySelector(t);e&&(this.activator=e),this.resetActivatorEvents(this.disabled,this.openOnHover,this.openOnFocus)}addActivatorEvents(){if(!this.activator||this.disabled)return;this.activatorListeners=this.genActivatorListeners();const t=Object.keys(this.activatorListeners);for(const e of t)this.activator.addEventListener(e,this.activatorListeners[e])}genActivatorListeners(){if(this.disabled)return{};const t={};return this.openOnHover?(t.mouseenter=t=>{this.runDelay("open")},t.mouseleave=t=>{this.runDelay("close")}):this.openOnClick&&(t.click=t=>{this.activator&&this.activator.focus(),t.stopPropagation(),this.dotNetHelper.invokeMethodAsync("OnClick",e(t)),this.setActive(!this.isActive)}),this.openOnFocus&&(t.focus=t=>{t.stopPropagation(),this.runDelay("open")},t.blur=t=>{this.runDelay("close")}),t}removeActivatorEvents(){if(!this.activator)return;const t=Object.keys(this.activatorListeners);for(const e of t)this.activator.removeEventListener(e,this.activatorListeners[e]);this.activatorListeners={}}resetActivatorEvents(t,e,s){this.disabled=t,this.openOnHover=e,this.openOnFocus=s,this.removeActivatorEvents(),this.addActivatorEvents()}runDelaying(t){this.runDelay(t?"open":"close")}registerPopup(t,e){const s=document.querySelector(t);s?(this.popupElement=s,this.closeOnContentClick=e,this.addPopupEvents()):console.error("popup not exists")}addPopupEvents(){if(!this.popupElement||this.disabled)return;this.popupListeners=this.genPopupListeners();const t=Object.keys(this.popupListeners);for(const e of t)this.popupElement.addEventListener(e,this.popupListeners[e])}removePopupEvents(){if(!this.popupElement)return;const t=Object.keys(this.popupListeners);for(const e of t)this.popupElement.removeEventListener(e,this.popupListeners[e]);this.popupListeners={}}genPopupListeners(){if(this.disabled)return;const t={};return!this.disabled&&this.openOnHover&&(t.mouseenter=t=>{this.runDelay("open")},t.mouseleave=t=>{this.runDelay("close")}),this.closeOnContentClick&&(t.click=t=>{this.setActive(!1)}),t}resetPopupEvents(t){this.closeOnContentClick=t,this.removePopupEvents(),this.addPopupEvents()}}function o(t,e,s,o,n,r,c,p,h){var a=new i(t,e,s,o,n,r,c,p,h);return a.addActivatorEvents(),a}export{o as init};
+import{d as t}from"../../chunks/helper.js";import{p as e}from"../../chunks/EventType.js";class s{constructor(t,e,s){this.openDelay=t,this.closeDelay=e,this.dotNetHelper=s}clearDelay(){clearTimeout(this.openTimeout),clearTimeout(this.closeTimeout)}runDelay(t,e){this.clearDelay();const s=parseInt(this[`${t}Delay`],10);this[`${t}Timeout`]=setTimeout(e||(()=>{const e={open:!0,close:!1}[t];this.setActive(e)}),s)}setActive(t){this.isActive!=t&&(this.isActive=t,this.dotNetHelper.invokeMethodAsync("SetActive",this.isActive))}resetDelay(t,e){this.openDelay=t,this.closeDelay=e}}class i extends s{constructor(e,s,i,o,n,r,c,a,h){super(c,a,h),this.activatorListeners={},this.popupListeners={},this.isActive=e,this.activator=t(s),this.disabled=i,this.openOnClick=o,this.openOnHover=n,this.openOnFocus=r,this.dotNetHelper=h}resetActivator(t){const e=document.querySelector(t);e&&(this.activator=e),this.resetActivatorEvents(this.disabled,this.openOnHover,this.openOnFocus)}addActivatorEvents(){if(!this.activator||this.disabled)return;this.activatorListeners=this.genActivatorListeners();const t=Object.keys(this.activatorListeners);for(const e of t)this.activator.addEventListener(e,this.activatorListeners[e])}genActivatorListeners(){if(this.disabled)return{};const t={};return this.openOnHover?(t.mouseenter=t=>{this.runDelay("open")},t.mouseleave=t=>{this.runDelay("close")}):this.openOnClick&&(t.click=t=>{var s,i;this.activator&&this.activator.focus();const o=t.composedPath().find((t=>t===this.activator));console.log("target",o,o._blazorEvents_1),(null===(i=null===(s=null==o?void 0:o._blazorEvents_1)||void 0===s?void 0:s.handlers)||void 0===i?void 0:i.click)||t.stopPropagation(),this.dotNetHelper.invokeMethodAsync("OnClick",e(t)),this.setActive(!this.isActive)}),this.openOnFocus&&(t.focus=t=>{var e,s;const i=t.composedPath().find((t=>t===this.activator));(null===(s=null===(e=null==i?void 0:i._blazorEvents_1)||void 0===e?void 0:e.handlers)||void 0===s?void 0:s.focus)||t.stopPropagation(),this.runDelay("open")},t.blur=t=>{this.runDelay("close")}),t}removeActivatorEvents(){if(!this.activator)return;const t=Object.keys(this.activatorListeners);for(const e of t)this.activator.removeEventListener(e,this.activatorListeners[e]);this.activatorListeners={}}resetActivatorEvents(t,e,s){this.disabled=t,this.openOnHover=e,this.openOnFocus=s,this.removeActivatorEvents(),this.addActivatorEvents()}runDelaying(t){this.runDelay(t?"open":"close")}registerPopup(t,e){const s=document.querySelector(t);s?(this.popupElement=s,this.closeOnContentClick=e,this.addPopupEvents()):console.error("popup not exists")}addPopupEvents(){if(!this.popupElement||this.disabled)return;this.popupListeners=this.genPopupListeners();const t=Object.keys(this.popupListeners);for(const e of t)this.popupElement.addEventListener(e,this.popupListeners[e])}removePopupEvents(){if(!this.popupElement)return;const t=Object.keys(this.popupListeners);for(const e of t)this.popupElement.removeEventListener(e,this.popupListeners[e]);this.popupListeners={}}genPopupListeners(){if(this.disabled)return;const t={};return!this.disabled&&this.openOnHover&&(t.mouseenter=t=>{this.runDelay("open")},t.mouseleave=t=>{this.runDelay("close")}),this.closeOnContentClick&&(t.click=t=>{this.setActive(!1)}),t}resetPopupEvents(t){this.closeOnContentClick=t,this.removePopupEvents(),this.addPopupEvents()}}function o(t,e,s,o,n,r,c,a,h){var p=new i(t,e,s,o,n,r,c,a,h);return p.addActivatorEvents(),p}export{o as init};
 //# sourceMappingURL=index.js.map

commit 74a514af7ca169018956b6b06dcb00b61514765b

🐛 (Button): bug introduced by #2332 that prevents button state from changing on click

src/Masa.Blazor/Components/Button/MButton.razor.cs
@@ -219,7 +219,8 @@ namespace Masa.Blazor
 #endif
 
             Attributes["ripple"] = Ripple;
-            if (OnClick.HasDelegate)
+
+            if (OnClick.HasDelegate || ItemGroup is not null)
             {
                 Attributes["onclick"] = EventCallback.Factory.Create<MouseEventArgs>(this, HandleOnClick);
             }