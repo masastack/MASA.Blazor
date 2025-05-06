7cb19e4f5c2dcb8d3c2871b7662f3339f0e33396
capdiem <capdiem@live.com>
2025-05-06 14:21:35 +0800

    update tests

--- test/Masa.Blazor.Test/Input/MInputTests.cs
+++ test/Masa.Blazor.Test/Input/MInputTests.cs
@@ -45,20 +45,6 @@ namespace Masa.Blazor.Test.Input
         }
 
         [TestMethod]
-        public void RenderInputNoWithWithDark()
-        {
-            //Act
-            var cut = RenderAndGetRootElement(props =>
-            {
-                props.Add(input => input.Dark, false);
-            });
-            var hasDarkClass = cut.ClassList.Contains("theme--light");
-
-            // Assert
-            Assert.IsTrue(hasDarkClass);
-        }
-
-        [TestMethod]
         public void RenderInputWithWithLight()
         {
             //Act
