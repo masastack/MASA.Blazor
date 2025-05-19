1d2b627aeeef8bcad2ba43bf32ed3ff76f02ddb7
capdiem <capdiem@live.com>
2025-05-19 09:04:38 +0800

    refactor: move the Xgplayer component to a standalone project (#2414)
    * 🚚 refactor: move the Xgplayer component to a standalone project
    
    * update the video url of demo
    
    * fix: unexpected quote
    
    * 🔧 chore: update push-nuget-package workflow
    
    * update docs

---
+++ src/Masa.Blazor.JSComponents.Xgplayer/JSInterops/XgplayerJSInteropHandle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace Masa.Blazor.Components.Xgplayer;
+
+public class XgplayerJSInteropHandle(MXgMusicPlayer player)
+{
+    [JSInvokable]
+    public Task OnFullscreenChange(bool isFullscreen)
+        => player.OnFullscreenChange.InvokeAsync(isFullscreen);
+
+    [JSInvokable]
+    public Task OnCssFullscreenChange(bool isCssFullscreen)
+        => player.OnCssFullscreenChange.InvokeAsync(isCssFullscreen);
+
+    [JSInvokable]
+    public Task OnFullscreenTouchend()
+        => player.OnFullscreenTouchend.InvokeAsync();
+}
---
+++ src/Masa.Blazor.JSComponents.Xgplayer/_Import.cs
@@ -0,0 +1,7 @@
+﻿// Global using directives
+
+global using System.Text.Json.Serialization;
+global using Masa.Blazor.Attributes;
+global using Masa.Blazor.JSInterop;
+global using Microsoft.AspNetCore.Components;
+global using OneOf;
