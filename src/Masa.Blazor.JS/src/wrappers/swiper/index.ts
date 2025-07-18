4291ec5b4af0fac48f1a84d0d16fd8e36e0d952c
capdiem <capdiem@live.com>
2025-07-18 10:56:51 +0800

    ♻ refactor(swiper): wrap the update method for blazor compatibility

--- src/Masa.Blazor.JS/src/wrappers/swiper/index.ts
+++ src/Masa.Blazor.JS/src/wrappers/swiper/index.ts
@@ -7,6 +7,11 @@ class SwiperProxy {
   handle: DotNet.DotNetObject;
   swiper: SwiperClass;
 
+  // This flag is used to solve the issue where Blazor has not finished rendering when calling the update method,
+  // resulting in no UI changes after the update. Previously, it required a manual delay(await Task.Delay(1000);) before
+  // calling the update method to resolve this issue.
+  updateAgainInSlidesUpdatedEvent: boolean;
+
   constructor(
     el: HTMLElement,
     swiperOptions: SwiperOptions,
@@ -36,7 +41,13 @@ class SwiperProxy {
     const swiper = new (Swiper as any)(el, swiperOptions);
     this.swiper = swiper;
     this.handle = handle;
-    this.swiper.on("activeIndexChange", (e) => this.onRealIndexChange(e, this));
+    this.swiper.on("activeIndexChange", e => this.onRealIndexChange(e, this));
+    this.swiper.on("slidesUpdated", e => {
+      if (this.updateAgainInSlidesUpdatedEvent) {
+        this.updateAgainInSlidesUpdatedEvent = false;
+        this.swiper.update();
+      }
+    });
 
     el._swiper = {
       instance: this.swiper,
@@ -56,6 +67,11 @@ class SwiperProxy {
     this.swiper.slidePrev(speed);
   }
 
+  update() {
+    this.updateAgainInSlidesUpdatedEvent = true;
+    this.swiper.update();
+  }
+
   dispose() {
     this.swiper && this.swiper.destroy(true);
     this.handle.dispose();
@@ -69,7 +85,10 @@ class SwiperProxy {
 
   async onRealIndexChange(e: SwiperClass, that: SwiperProxy) {
     if (that.handle) {
-      await that.handle.invokeMethodAsync("OnIndexChanged", e.originalParams.loop ? e.realIndex : e.activeIndex);
+      await that.handle.invokeMethodAsync(
+        "OnIndexChanged",
+        e.originalParams.loop ? e.realIndex : e.activeIndex
+      );
     }
   }
 }
