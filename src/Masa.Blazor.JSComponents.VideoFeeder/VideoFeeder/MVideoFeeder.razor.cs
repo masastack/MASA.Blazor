5d3b834df81ba3af3149e20c29810c78728e69ab
capdiem <capdiem@live.com>
2025-07-31 16:27:22 +0800

    update

--- src/Masa.Blazor.JSComponents.VideoFeeder/VideoFeeder/MVideoFeeder.razor.cs
+++ src/Masa.Blazor.JSComponents.VideoFeeder/VideoFeeder/MVideoFeeder.razor.cs
@@ -218,7 +218,7 @@ public partial class MVideoFeeder<TItem> where TItem : notnull
     [MasaApiPublicMethod]
     public async Task<XgplayerPropsAndStates?> GetCurrentVideoPropsAndStatesAsync()
     {
-        return CurrentVideo is null ? null : await CurrentVideo.Player?.GetPropsAndStatesAsync();
+        return CurrentVideo?.Player is null ? null : await CurrentVideo.Player.GetPropsAndStatesAsync();
     }
 
     private async Task ToggleMode()
