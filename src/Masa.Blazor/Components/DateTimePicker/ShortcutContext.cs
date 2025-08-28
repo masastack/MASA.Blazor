608df079fd79884ab008f36e8bf11de3f57bd1c6
capdiem <capdiem@live.com>
2025-08-28 16:38:46 +0800

    🆕 feat(DateTimePicker): support for addings shortcuts for quick select datetime

---
+++ src/Masa.Blazor/Components/DateTimePicker/ShortcutContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Masa.Blazor.Components.DateTimePicker;
+
+/// <summary>
+/// Shortcut context for date/time picker shortcuts
+/// </summary>
+/// <param name="Update">An action to update the selected date/time</param>
+public record ShortcutContext(Action<DateTime> Update);
